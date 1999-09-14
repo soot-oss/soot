560e129819c971dec4b28603267888a778129b21
Raja Vallee-Rai <rvalleerai@sable.mcgill.ca>
1999-09-14 16:30:25 +0000

    prcs 1.beta.5.dev.21: empty

--- src/soot/Main.java
+++ src/soot/Main.java
@@ -174,7 +174,7 @@ public class Main
         if(args.length == 0)
         {
 // $Format: "            System.out.println(\"Soot version $ProjectVersion$\");"$
-            System.out.println("Soot version 1.beta.5.dev.20");
+            System.out.println("Soot version 1.beta.5.dev.21");
             System.out.println("Copyright (C) 1997-1999 Raja Vallee-Rai (rvalleerai@sable.mcgill.ca).");
             System.out.println("All rights reserved.");
             System.out.println("");
