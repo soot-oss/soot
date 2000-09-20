85e316b625e5f4576d738d0419ffe740e2ffd5c3
Feng Qian <fqian@sable.mcgill.ca>
2000-09-20 16:28:52 +0000

    prcs 1.0.0.dev.48: empty

--- src/soot/Scene.java
+++ src/soot/Scene.java
@@ -153,8 +153,8 @@ public class Scene  //extends AbstractHost
         // Whole-Jimple Optimization pack (--app -W)
         packNameToPack.put("wjop", p = new Pack());
         {
-            p.add(new Transform("wjop.smb", StaticMethodBinder.v()));
-            p.add(new Transform("wjop.si", StaticInliner.v(), "disabled"));
+            p.add(new Transform("wjop.smb", StaticMethodBinder.v(), "disabled"));
+            p.add(new Transform("wjop.si", StaticInliner.v()));
         }
 
 	// Give another chance to do Whole-Jimple transformation
