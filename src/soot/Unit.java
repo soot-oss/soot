8d9e06886e50c5df56fc4896497a94a4c95273e5
John Jorgensen <jjorge1@sable.mcgill.ca>
2003-05-16 03:19:53 +0000

    Clarified (I hope) javadoc comment.

--- src/soot/Unit.java
+++ src/soot/Unit.java
@@ -65,9 +65,9 @@ public interface Unit extends Switchable, Host, Serializable
     public Object clone();
 
     /** Returns true if execution after this statement may continue at the following statement.
-     * GotoStmt will return false. */
+     * GotoStmt will return false but IfStmt will return true. */
     public boolean fallsThrough();
-    /** Returns true if execution after this statement is not the following statement. */
+    /** Returns true if execution after this statement does not necessarily continue at the following statement. GotoStmt and IfStmt will both return true. */
     public boolean branches();        
     
     public void toString(UnitPrinter up);
