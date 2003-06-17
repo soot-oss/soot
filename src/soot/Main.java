a242658669ab0415375f4bbef147bfe2e7c82d83
Ondrej Lhoták <olhotak@uwaterloo.ca>
2003-06-17 11:04:26 +0000

    Bump version number to 2.0.1

--- src/soot/Main.java
+++ src/soot/Main.java
@@ -49,7 +49,7 @@ public class Main {
     // TODO: the following string should be updated by the source control
     // No it shouldn't. Prcs is horribly borken in this respect, and causes
     // the code to not compile all the time.
-    public final String versionString = "2.0";
+    public final String versionString = "2.0.1";
 
     private Date start;
     private Date finish;
