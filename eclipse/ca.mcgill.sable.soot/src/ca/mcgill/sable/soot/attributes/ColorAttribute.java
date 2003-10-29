859b9314bd565f726559b907e2133a6106632fc8
Jennifer Lhoták <jlhotak@sable.mcgill.ca>
2003-10-29 02:38:37 +0000

    -forgot to add this file

---
+++ eclipse/ca.mcgill.sable.soot/src/ca/mcgill/sable/soot/attributes/ColorAttribute.java
@@ -0,0 +1,40 @@
+package ca.mcgill.sable.soot.attributes;
+
+import soot.*;
+
+public class ColorAttribute{
+            
+    private int red;
+    private int green;
+    private int blue;
+    private int fg;
+
+    public ColorAttribute(int red, int green, int blue, boolean fg){
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        if (fg){
+            this.fg = 1;
+        }
+        else {
+            this.fg = 0;
+        }
+    }
+
+    public int red(){
+        return red;
+    }
+
+    public int green(){
+        return green;
+    }
+
+    public int blue(){
+        return blue;
+    }
+
+    public int fg(){
+        return fg;
+    }
+
+}
