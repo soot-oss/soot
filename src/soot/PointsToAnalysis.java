9e0d819499769819d5bc5bfbb7dcc95465ca4427
Ondrej Lhoták <olhotak@uwaterloo.ca>
2004-03-26 05:01:27 +0000

    Added descriptive strings to PointsToAnalysis magic constants (so you see THIS_NODE instead of -1 in dumps).

--- src/soot/PointsToAnalysis.java
+++ src/soot/PointsToAnalysis.java
@@ -42,25 +42,25 @@ public interface PointsToAnalysis {
      * in the PointsToSet s. */
     public PointsToSet reachingObjectsOfArrayElement( PointsToSet s );
 
-    public static final Integer THIS_NODE = new Integer( -1 );
+    public static final String THIS_NODE = "THIS_NODE";
     public static final int RETURN_NODE = -2;
-    public static final Integer THROW_NODE = new Integer( -3 );
-    public static final Integer ARRAY_ELEMENTS_NODE = new Integer( -4 );
-    public static final Integer CAST_NODE = new Integer( -5 );
-    public static final Integer STRING_ARRAY_NODE = new Integer( -6 );
-    public static final Integer STRING_NODE = new Integer( -7 );
-    public static final Integer STRING_NODE_LOCAL = new Integer( -8 );
-    public static final Integer EXCEPTION_NODE = new Integer( -9 );
-    public static final Integer RETURN_STRING_CONSTANT_NODE = new Integer( -10 );
-    public static final Integer STRING_ARRAY_NODE_LOCAL = new Integer( -11 );
-    public static final Integer MAIN_THREAD_NODE = new Integer( -12 );
-    public static final Integer MAIN_THREAD_NODE_LOCAL = new Integer( -13 );
-    public static final Integer MAIN_THREAD_GROUP_NODE = new Integer( -14 );
-    public static final Integer MAIN_THREAD_GROUP_NODE_LOCAL = new Integer( -15 );
-    public static final Integer MAIN_CLASS_NAME_STRING = new Integer( -16 );
-    public static final Integer MAIN_CLASS_NAME_STRING_LOCAL = new Integer( -17 );
-    public static final Integer DEFAULT_CLASS_LOADER = new Integer( -18 );
-    public static final Integer DEFAULT_CLASS_LOADER_LOCAL = new Integer( -19 );
+    public static final String THROW_NODE = "THROW_NODE";
+    public static final String ARRAY_ELEMENTS_NODE = "ARRAY_ELEMENTS_NODE";
+    public static final String CAST_NODE = "CAST_NODE";
+    public static final String STRING_ARRAY_NODE = "STRING_ARRAY_NODE";
+    public static final String STRING_NODE = "STRING_NODE";
+    public static final String STRING_NODE_LOCAL = "STRING_NODE_LOCAL";
+    public static final String EXCEPTION_NODE = "EXCEPTION_NODE";
+    public static final String RETURN_STRING_CONSTANT_NODE = "RETURN_STRING_CONSTANT_NODE";
+    public static final String STRING_ARRAY_NODE_LOCAL = "STRING_ARRAY_NODE_LOCAL";
+    public static final String MAIN_THREAD_NODE = "MAIN_THREAD_NODE";
+    public static final String MAIN_THREAD_NODE_LOCAL = "MAIN_THREAD_NODE_LOCAL";
+    public static final String MAIN_THREAD_GROUP_NODE = "MAIN_THREAD_GROUP_NODE";
+    public static final String MAIN_THREAD_GROUP_NODE_LOCAL = "MAIN_THREAD_GROUP_NODE_LOCAL";
+    public static final String MAIN_CLASS_NAME_STRING = "MAIN_CLASS_NAME_STRING";
+    public static final String MAIN_CLASS_NAME_STRING_LOCAL = "MAIN_CLASS_NAME_STRING_LOCAL";
+    public static final String DEFAULT_CLASS_LOADER = "DEFAULT_CLASS_LOADER";
+    public static final String DEFAULT_CLASS_LOADER_LOCAL = "DEFAULT_CLASS_LOADER_LOCAL";
 
 }
 
