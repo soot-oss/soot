459aee3bd1de3605d0e02d277ff45af4252b3326
Ondrej Lhoták <olhotak@uwaterloo.ca>
2004-12-17 20:30:29 +0000

    changed location to nightly builds to soot/nightly

--- src/soot/EntryPoints.java
+++ src/soot/EntryPoints.java
@@ -78,7 +78,7 @@ public class EntryPoints
         //addMethod( ret, "<java.lang.ThreadGroup: void remove(java.lang.Thread)>");
         addMethod( ret, "<java.lang.Thread: void exit()>");
         addMethod( ret, "<java.lang.ThreadGroup: void uncaughtException(java.lang.Thread,java.lang.Throwable)>");
-        addMethod( ret, "<java.lang.System: void loadLibrary(java.lang.String)>");
+        //addMethod( ret, "<java.lang.System: void loadLibrary(java.lang.String)>");
         addMethod( ret, "<java.lang.ClassLoader: void <init>()>");
         addMethod( ret, "<java.lang.ClassLoader: java.lang.Class loadClassInternal(java.lang.String)>");
         addMethod( ret, "<java.lang.ClassLoader: void checkPackageAccess(java.lang.Class,java.security.ProtectionDomain)>");
