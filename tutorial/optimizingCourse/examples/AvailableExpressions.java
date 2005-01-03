b011c885feb078c92c5e9716d107438ee1b80a6e
Jennifer Lhoták <jlhotak@sable.mcgill.ca>
2005-01-03 22:04:21 +0000

    - add example - update slides

---
+++ tutorial/optimizingCourse/examples/AvailableExpressions.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Dec 30, 2004
+ */
+
+/**
+ * @author jlhotak
+ */
+public class AvailableExpressions {
+
+	public static void main(String[] args) {
+		int a = 0; int b = 0; int c = 0;
+		int d = 0; int u = 0; int v = 0;
+		int x = 0; int y = 0;
+		
+		u = a + b;
+		v = c + d;
+		while (x < 100){
+			x = b + c;
+			y = c + d; 
+		}
+		c = 3;
+		
+	}
+}
