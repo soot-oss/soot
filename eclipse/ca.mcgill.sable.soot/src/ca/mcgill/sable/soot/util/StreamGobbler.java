8af895504fbf568e06475548454229ebef14ed7a
Jennifer Lhoták <jlhotak@sable.mcgill.ca>
2005-08-12 20:12:30 +0000

    -remove printlns, deadcode

--- eclipse/ca.mcgill.sable.soot/src/ca/mcgill/sable/soot/util/StreamGobbler.java
+++ eclipse/ca.mcgill.sable.soot/src/ca/mcgill/sable/soot/util/StreamGobbler.java
@@ -17,24 +17,6 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/**
- * @author jlhotak
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
 
 package ca.mcgill.sable.soot.util;
 
@@ -65,7 +47,6 @@ public class StreamGobbler extends Thread {
 	}
 	
 	public void run() {
-		//System.out.println("Gobbler running");
 		try {
 			InputStreamReader isr = new InputStreamReader(getIs());
 			BufferedReader br = new BufferedReader(isr);
@@ -97,7 +78,7 @@ public class StreamGobbler extends Thread {
 			}
 		}
 		catch(IOException e1) {
-			//System.out.println(e1.getMessage());
+			System.out.println(e1.getMessage());
 		}
 	}
 	
