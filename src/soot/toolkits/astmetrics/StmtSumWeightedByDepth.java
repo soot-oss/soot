22d7cc5cfb310ae8d04c0f47d62fe99eb19229b3
Michael Batchelder <mbatch@cs.mcgill.ca>
2006-03-09 18:49:54 +0000

    Added Node counting Added blocks in blocks depth complexity check Removed return stmt depth sum

--- src/soot/toolkits/astmetrics/StmtSumWeightedByDepth.java
+++ src/soot/toolkits/astmetrics/StmtSumWeightedByDepth.java
@@ -1,92 +1,136 @@
 package soot.toolkits.astmetrics;
 
+import java.util.*;
 import polyglot.ast.*;
 import polyglot.visit.NodeVisitor;
 
 public class StmtSumWeightedByDepth extends ASTMetric {
-
-	int currentDepth;
-	int sum;
-	int maxDepth;
-	int returnSum;
-	
-	
-	public StmtSumWeightedByDepth(Node node){
-		super(node);
-	}
-	
-	
-	public void reset() {
-		// TODO Auto-generated method stub
-		currentDepth = 1; //inside a class
-		maxDepth = 1;
-		sum = 0;
-		returnSum =0;
-	}
-
-	public void addMetrics(ClassData data) {
-		// TODO Auto-generated method stub
-		data.addMetric(new MetricData("MaxDepth",new Integer(maxDepth)));
-		data.addMetric(new MetricData("D-W-Complexity",new Integer(sum)));
-		data.addMetric(new MetricData("Return-Depth-Sum",new Integer(returnSum)));
-	}
-
-	private void increaseDepth(){
-		currentDepth++;
-		if(currentDepth > maxDepth)
-			maxDepth = currentDepth;
-	}
-	
-	private void decreaseDepth(){
-		currentDepth--;
-	}
-	
-
-	/*
-	 * List of Node types which increase depth of traversal!!!
-	 * Any construct where one can have a { } increases the depth
-	 * hence even though if(cond) stmt doesnt expicitly use a block 
-	 * its depth is still +1 when executing the stmt
-	 * 
-	 * If the "if" stmt has code if(cond) { stmt } this will actually increase the depth by 2 first by the if and then by the block
-	 * 
-	 * If .... add currentDepth to sum and then increase depth by one for both then and else branch irrespective of how many stmts there are in the body
-	 * Loop (Takes care of do while and for): Add currentDepth then increment by 1
-	 * Block ... add currentDepth plus increment depth
-	 * LocalClassDecl.... add currentDepth plus increment since everything inside is more complex
-	 * 
-	 * Try ... add currentDepth (Dont incremenet as this will be done by the respective BLOCK as all try catch and finally are blocks!!
-	 * Synchronized ... add currentDepth....dont increment depth as this will be done by the block as synchs are always blocks)
-	 * ProcedureDecl (methods and constructors. thats where currentDepth is ONE as these belong to the class) ...
-	 * Initializer (Handles static and non static class level chunks of code)
-	 * Switch .... add current Depth however since each case is necessarily a block dont increment depth that WILL get incremented
-	 *       
-	 */
-	public NodeVisitor enter(Node parent, Node n){
-		if(n instanceof If || n instanceof Loop || n instanceof Block || n instanceof LocalClassDecl){
-			sum += currentDepth*3;
-			increaseDepth();
-		}
-		else if(n instanceof Try || n instanceof Synchronized || n instanceof ProcedureDecl || n instanceof Initializer || n instanceof Switch){
-			sum+= currentDepth*3;
-		}
-		else{
-			if(n instanceof Return){
-				returnSum += currentDepth;
-				System.out.println("RETURN111111111111111111111111111111111"+currentDepth);
-			}
-			
-			sum+= currentDepth;
-		}
-		return enter(n);
-	}
- 
-	 
-	public Node leave(Node old, Node n, NodeVisitor v){
-		if(n instanceof If || n instanceof Loop || n instanceof Block || n instanceof LocalClassDecl){
-			decreaseDepth();
-		}
-		return n;
-	}
-	
+  
+  int currentDepth;
+  int sum;
+  int maxDepth;
+  int numNodes;
+  //int returnSum;
+  
+  Stack labelNodesSoFar = new Stack();
+  ArrayList blocksWithAbruptFlow = new ArrayList();
+  
+  public static boolean tmpAbruptChecker = false; 
+  
+  public StmtSumWeightedByDepth(Node node){
+    super(node);
+  }
+  
+  
+  public void reset() {
+    currentDepth = 1; //inside a class
+    maxDepth = 1;
+    sum = 0;
+    numNodes = 0;
+    //returnSum =0;
+  }
+  
+  public void addMetrics(ClassData data) {
+    data.addMetric(new MetricData("MaxDepth",new Integer(maxDepth)));
+    data.addMetric(new MetricData("D-W-Complexity",new Integer(sum)));
+    data.addMetric(new MetricData("AST-Node-Count",new Integer(numNodes)));
+    //data.addMetric(new MetricData("Return-Depth-Sum",new Integer(returnSum)));
+  }
+  
+  private void increaseDepth(){
+    currentDepth++;
+    if(currentDepth > maxDepth)
+      maxDepth = currentDepth;
+  }
+  
+  private void decreaseDepth(){
+    currentDepth--;
+  }
+  
+  
+  /*
+   * List of Node types which increase depth of traversal!!!
+   * Any construct where one can have a { } increases the depth
+   * hence even though if(cond) stmt doesnt expicitly use a block 
+   * its depth is still +1 when executing the stmt
+   * 
+   * If the "if" stmt has code if(cond) { stmt } OR if(cond) stmt this will only increase the depth by 1 (ignores compound stmt blocks)
+   * 
+   * If, Loop, Try, Synch, ProcDecl, Init, Switch, LocalClassDecl .... add currentDepth to sum and then increase depth by one
+   * irrespective of how many stmts there are in the body
+   * 
+   * Block ... if it is a block within a block, add currentDepth plus increment depth ONLY if it has abrupt flow out of it.
+   */
+  public NodeVisitor enter(Node parent, Node n){
+    numNodes++;
+    if (n instanceof CodeDecl) {
+      // maintain stack of label arrays (can't have label from inside method to outside)
+      labelNodesSoFar.push(new ArrayList());
+    }
+    else if (n instanceof Labeled) {
+      // add any labels we find to the array
+      ((ArrayList)labelNodesSoFar.peek()).add(((Labeled)n).label());
+    }
+    
+    if(n instanceof If || n instanceof Loop || n instanceof Try || n instanceof Switch
+        || n instanceof LocalClassDecl || n instanceof Synchronized 
+        || n instanceof ProcedureDecl || n instanceof Initializer ){
+      sum += currentDepth*2;
+      increaseDepth();
+    } else if (parent instanceof Block && n instanceof Block) {
+      StmtSumWeightedByDepth.tmpAbruptChecker = false;
+      n.visit(new NodeVisitor() {
+        // extended NodeVisitor that checks for branching out of a block
+        public NodeVisitor enter(Node parent, Node node){
+          if(node instanceof Branch) {
+            Branch b = (Branch)node;
+            // null branching out of a plain block is NOT ALLOWED!
+            if (b.label() != null && ((ArrayList)labelNodesSoFar.peek()).contains(b.label()))
+            {
+              StmtSumWeightedByDepth.tmpAbruptChecker = true;
+            }
+          }
+          return enter(node);
+        }
+        // this method simply stops further node visiting if we found our info
+        public Node override(Node parent, Node node) {
+          if (StmtSumWeightedByDepth.tmpAbruptChecker)
+            return node;
+          return null;
+        }
+      });
+      
+      if (StmtSumWeightedByDepth.tmpAbruptChecker)
+      {
+        blocksWithAbruptFlow.add(n);
+        sum += currentDepth*2;
+        increaseDepth();
+      }
+    } else {
+      //if(n instanceof Return){
+      //	returnSum += currentDepth;
+      //	System.out.println("RETURN111111111111111111111111111111111"+currentDepth);
+      //}
+      sum+= currentDepth;
+    }
+    
+    return enter(n);
+  }
+  
+  
+  public Node leave(Node old, Node n, NodeVisitor v){
+    
+    // stack maintenance, if leaving a method
+    if (n instanceof CodeDecl)
+      labelNodesSoFar.pop();
+    
+    if(n instanceof If || n instanceof Loop || n instanceof Try || n instanceof Switch
+        || n instanceof LocalClassDecl || n instanceof Synchronized 
+        || n instanceof ProcedureDecl || n instanceof Initializer ) {
+      decreaseDepth();
+    } else if (n instanceof Block && blocksWithAbruptFlow.contains(n)) {
+      decreaseDepth();
+    }
+    return n;
+  }
 }
