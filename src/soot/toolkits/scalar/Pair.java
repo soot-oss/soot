538dfb6ca7617714176cf81a9fe4e32993ea0091
Eric Bodden <bodden@ec-spride.de>
2012-01-21 15:48:11 +0000

    compute equals/hashCode correctly even in case of null values

--- src/soot/toolkits/scalar/Pair.java
+++ src/soot/toolkits/scalar/Pair.java
@@ -32,15 +32,38 @@ public class Pair<T, U>
 {
 	public Pair() { o1 = null; o2 = null; }
     public Pair( T o1, U o2 ) { this.o1 = o1; this.o2 = o2; }
-    public int hashCode() {
-        return o1.hashCode() + o2.hashCode();
-    }
-    public boolean equals( Object other ) {
-        if( other instanceof Pair) {
-            Pair p = (Pair) other;
-            return o1.equals( p.o1 ) && o2.equals( p.o2 );
-        } else return false;
-    }
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((o1 == null) ? 0 : o1.hashCode());
+		result = prime * result + ((o2 == null) ? 0 : o2.hashCode());
+		return result;
+	}
+    
+    @Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		@SuppressWarnings("rawtypes")
+		Pair other = (Pair) obj;
+		if (o1 == null) {
+			if (other.o1 != null)
+				return false;
+		} else if (!o1.equals(other.o1))
+			return false;
+		if (o2 == null) {
+			if (other.o2 != null)
+				return false;
+		} else if (!o2.equals(other.o2))
+			return false;
+		return true;
+	}
 
 	/**
 	 * Decide if this pair represents a method parameter.
