561d1f26bb6d6c55fe496049dcea668263486b9b
Alexandre Bartel <alexandre.bartel@uni.lu>
2012-07-03 11:34:14 +0000

    Tagged instruction abstract class.

---
+++ src/soot/dex/instructions/TaggedInstruction.java
@@ -0,0 +1,32 @@
+package soot.dex.instructions;
+import soot.dex.tags.DoubleOpTag;
+import soot.dex.tags.FloatOpTag;
+import soot.dex.tags.IntOpTag;
+import soot.dex.tags.LongOpTag;
+
+import org.jf.dexlib.Code.Instruction;
+
+import soot.tagkit.Tag;
+
+public abstract class TaggedInstruction extends DexlibAbstractInstruction {
+
+  private Tag instructionTag = null;
+
+  public TaggedInstruction(Instruction instruction, int codeAddress) {
+    super(instruction, codeAddress);
+    // TODO Auto-generated constructor stub
+  }
+
+  public void setTag (Tag t) {
+    instructionTag = t;
+  }
+  
+  public Tag getTag () {
+    if (instructionTag == null) {
+      System.err.println("Must tag instruction first! (0x"+ Integer.toHexString(codeAddress) +": "+ instruction +")");
+      System.exit(-1);
+    }
+    return instructionTag;
+  }
+
+}
