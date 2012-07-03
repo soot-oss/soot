02f2413beb692b3969389b1ee35057d313db4cf5
Eric Bodden <eric.bodden@ec-spride.de>
2012-07-03 11:10:39 +0200

    .

--- src/soot/util/IterableNumberer.java
+++ src/soot/util/IterableNumberer.java
@@ -23,7 +23,7 @@ import java.util.*;
 /** A numberer which also supports an iterator on newly-added objects.
  * @author xiao, generalize the interface
  */
-public interface IterableNumberer<E> extends Numberer<E> {
+public interface IterableNumberer<E> extends Numberer<E>, Iterable<E> {
     /** Returns an iterator over all objects added to the numberer. */
     Iterator iterator(); 
 }
