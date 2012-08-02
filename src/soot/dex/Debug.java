9caa9aa323350486678ccdab67d19c09c50c0f18
Alexandre Bartel <alexandre.bartel@uni.lu>
2012-08-02 07:21:39 +0000

    Debug activated with Soot's -v option.

--- src/soot/dex/Debug.java
+++ src/soot/dex/Debug.java
@@ -1,9 +1,12 @@
 package soot.dex;
 
+import soot.options.Options;
+
 public class Debug {
-  public static boolean DEXPLER_DEBUG = false;
+  public static boolean DEXPLER_DEBUG;
   
   public static void printDbg (String s) {
+    DEXPLER_DEBUG = Options.v().verbose();
     if (DEXPLER_DEBUG)
       System.out.println (s);
   }
