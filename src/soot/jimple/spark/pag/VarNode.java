fe673091e377a7c462c1f9627eb39962baf10b7b
richardxx <frogxx@gmail.com>
2013-02-02 01:52:14 +0800

    Change back the edits to VarNode.java. Add change log for geometric points-to solver.

--- src/soot/jimple/spark/pag/VarNode.java
+++ src/soot/jimple/spark/pag/VarNode.java
@@ -102,9 +102,9 @@ public abstract class VarNode extends ValNode implements Comparable {
 
     VarNode( PAG pag, Object variable, Type t ) {
 	super( pag, t );
-//	if( !(t instanceof RefLikeType) || t instanceof AnySubType ) {
-//	    throw new RuntimeException( "Attempt to create VarNode of type "+t );
-//	}
+	if( !(t instanceof RefLikeType) || t instanceof AnySubType ) {
+	    throw new RuntimeException( "Attempt to create VarNode of type "+t );
+	}
 	this.variable = variable;
         pag.getVarNodeNumberer().add(this);
         setFinishingNumber( ++pag.maxFinishNumber );
