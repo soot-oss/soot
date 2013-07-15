143f48e998332f87267c18dfce15ec97a387206e
Steven Arzt <Steven.Arzt@cased.de>
2013-07-15 13:19:29 +0200

    fixed a wrong register allocation in toDex in cases in which the IdentityStmts in a Jimple body were not in a linear increasing order - an assumption toDex shouldn't make anyway

--- src/soot/toDex/RegisterAllocator.java
+++ src/soot/toDex/RegisterAllocator.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import soot.Local;
+import soot.SootMethod;
 import soot.Type;
 import soot.Value;
 import soot.jimple.Constant;
@@ -55,11 +56,26 @@ public class RegisterAllocator {
 		return localRegister;
 	}
 
-	public void asParameter(Local l) {
+	public void asParameter(SootMethod sm, Local l) {
 		// since a parameter in dex always has a register, we handle it like a new local without the need of a new register
-		localToLastRegNum.put(l.getName(), nextRegNum);
+		// Register allocation is fixed! 0 for this, 1...n for parameters. We do not expect
+		// the IdentityStmts in the body in any fixed order, so we directly calculate
+		// the correct register number.
+		int paramRegNum = -1;
+		if (!sm.isStatic() && sm.getActiveBody().getThisLocal() == l)
+			paramRegNum = 0;
+		else
+			for (int i = 0; i < sm.getParameterCount(); i++)
+				if (sm.getActiveBody().getParameterLocal(i) == l) {
+					paramRegNum = i + 1;
+					break;
+				}
+		if (paramRegNum < 0)
+			throw new RuntimeException("Parameter local not found");
+		
+		localToLastRegNum.put(l.getName(), paramRegNum);
 		int wordsforParameters = SootToDexUtils.getDexWords(l.getType());
-		nextRegNum += wordsforParameters;
+		nextRegNum = Math.max(nextRegNum + wordsforParameters, paramRegNum + wordsforParameters);
 		paramRegCount += wordsforParameters;
 	}
 
