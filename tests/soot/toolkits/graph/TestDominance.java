2f2d1ff6146f9b179bf6fdbb46efdd1892efcc52
Tim Henderson <tim.tadh@gmail.com>
2014-03-21 14:27:23 -0400

    added dominator tree test for general acyclic region
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- tests/soot/toolkits/graph/TestDominance.java
+++ tests/soot/toolkits/graph/TestDominance.java
@@ -43,9 +43,28 @@ import soot.toolkits.graph.*;
 
 public class TestDominance {
 
+    public Set<Integer> kid_ids(DominatorNode dn) {
+        Set<Integer> kids = new HashSet<Integer>();
+        for (Object o : dn.getChildren()) {
+            DominatorNode dkid = (DominatorNode)o;
+            Node kid = (Node)dkid.getGode();
+            kids.add(kid.id);
+        }
+        return kids;
+    }
+
+    public Map<Integer,DominatorNode> kid_map(DominatorNode dn) {
+        Map<Integer,DominatorNode> kids = new HashMap<Integer,DominatorNode>();
+        for (Object o : dn.getChildren()) {
+            DominatorNode dkid = (DominatorNode)o;
+            Node kid = (Node)dkid.getGode();
+            kids.put(kid.id, dkid);
+        }
+        return kids;
+    }
+
     @Test
-    public void TestHello() {
-        System.out.println("hello world");
+    public void TestSimpleDiamond() {
         Graph g = new Graph(new Node(1));
         Node x = new Node(4);
         g.root.addkid((new Node(2)).addkid(x)).addkid((new Node(3)).addkid(x));
@@ -55,11 +74,92 @@ public class TestDominance {
         assertThat(tree.getHeads().size(), is(1));
         DominatorNode head = tree.getHeads().get(0);
         assertThat(((Node)head.getGode()).id, is(1));
-        List<DominatorNode> kids = head.getChildren();
+        Set<Integer> kids = kid_ids(head);
         assertThat(kids.size(), is(3));
-        assertThat(((Node)kids.get(0).getGode()).id, is(2));
-        assertThat(((Node)kids.get(1).getGode()).id, is(4));
-        assertThat(((Node)kids.get(2).getGode()).id, is(3));
+        assertThat(kids, contains(2, 3, 4));
+    }
+
+    @Test
+    public void AcyclicCFG() {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        Node n7 = new Node(7);
+        Node n8 = new Node(8);
+        Node n9 = new Node(9);
+        Node n10 = new Node(10);
+        Node n11 = new Node(11);
+        n1.addkid(n2).addkid(n3);
+        n2.addkid(n9);
+        n3.addkid(n4).addkid(n5);
+        n4.addkid(n9);
+        n5.addkid(n6).addkid(n10);
+        n6.addkid(n7).addkid(n8);
+        n7.addkid(n10);
+        n8.addkid(n10);
+        n9.addkid(n11);
+        n10.addkid(n11);
+        Graph g = new Graph(n1);
+
+        MHGDominatorsFinder finder = new MHGDominatorsFinder(g);
+        DominatorTree tree = new DominatorTree(finder);
+        assertThat(tree.getHeads().size(), is(1));
+
+        DominatorNode n = tree.getHeads().get(0);
+        assertThat(((Node)n.getGode()).id, is(1));
+        Set<Integer> kids = kid_ids(n);
+        assertThat(kids.size(), is(4));
+        assertThat(kids, contains(2, 3, 9, 11));
+
+        Map<Integer, DominatorNode> KM = kid_map(n);
+        DominatorNode m = KM.get(2);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
+
+        m = KM.get(9);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
+
+        m = KM.get(11);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
+
+        n = KM.get(3);
+        kids = kid_ids(n);
+        assertThat(kids.size(), is(2));
+        assertThat(kids, contains(4, 5));
+
+        KM = kid_map(n);
+        m = KM.get(4);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
+
+        n = KM.get(5);
+        kids = kid_ids(n);
+        assertThat(kids.size(), is(2));
+        assertThat(kids, contains(6, 10));
+
+        KM = kid_map(n);
+        m = KM.get(10);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
+
+        n = KM.get(6);
+        kids = kid_ids(n);
+        assertThat(kids.size(), is(2));
+        assertThat(kids, contains(7, 8));
+
+        KM = kid_map(n);
+        m = KM.get(7);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
+
+        m = KM.get(8);
+        kids = kid_ids(m);
+        assertThat(kids.size(), is(0));
     }
 }
 
