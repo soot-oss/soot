eefbbdfb8da7a73d224c003c13f7c840c245b43d
Steven Arzt <Steven.Arzt@cased.de>
2014-09-23 17:56:22 +0200

    fix for the fix

--- src/soot/asm/SootClassBuilder.java
+++ src/soot/asm/SootClassBuilder.java
@@ -174,10 +174,6 @@ class SootClassBuilder extends ClassVisitor {
 	@Override
 	public void visitInnerClass(String name, String outerName, String innerName, int access) {
 		klass.addTag(new InnerClassTag(name, outerName, innerName, access));
-		if (outerName != null) {
-			SootClass innerClass = SootResolver.v().makeClassRef(AsmUtil.toQualifiedName(name));
-			innerClass.setOuterClass(SootResolver.v().makeClassRef(AsmUtil.toQualifiedName(outerName)));
-		}
 	}
 	
 	@Override
