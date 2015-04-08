083dcc0bb7afb5c98e76750cc89c0ef280630b48
Florian Kübler <florian-kuebler@web.de>
2015-04-08 15:27:12 +0200

    Refactored array copy

--- src/soot/util/annotations/AnnotationInstanceCreator.java
+++ src/soot/util/annotations/AnnotationInstanceCreator.java
@@ -1,9 +1,8 @@
 package soot.util.annotations;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,54 +32,45 @@ public class AnnotationInstanceCreator {
 
 				map.put(result.getKey(), result.getValue());
 			}
-			
-			Object result = Proxy.newProxyInstance(cl, new Class[]{clazz}, new AbstractInvocationHandler() {
-				
+
+			Object result = Proxy.newProxyInstance(cl, new Class[] { clazz }, new AbstractInvocationHandler() {
+
+				@SuppressWarnings("unchecked")
 				@Override
 				protected Object handleInvocation(Object proxy, Method method, Object[] args) throws Throwable {
 					String name = method.getName();
-					Class<?> retType =  method.getReturnType();
-					if (name.equals("annotationType")){
+					Class<?> retType = method.getReturnType();
+					if (name.equals("annotationType")) {
 						return clazz;
 					}
-					
+
 					Object result = map.get(name);
-					
-					
-					if (result != null){
-						if(result instanceof Object[]){
+
+					if (result != null) {
+						if (result instanceof Object[]) {
 							Object[] oa = (Object[]) result;
-							Object array = Array.newInstance(retType.getComponentType(), oa.length);
-							
-							int i = 0;
-							for(Object o : oa){
-								Array.set(array, i, o);
-								i++;
-							}
-							
-							return array;
+
+							return Arrays.copyOf(oa, oa.length, (Class<? extends Object[]>) retType);
 						}
-						
+
 						if ((retType.equals(boolean.class) || retType.equals(Boolean.class)) && (result instanceof Integer)) {
-							return ((Integer) result).intValue() == 0 ? Boolean.FALSE : Boolean.TRUE;
+							return ((Integer) result) != 0;
 						}
-						
+
 						return result;
 					} else {
 						result = method.getDefaultValue();
-						if (result != null){
+						if (result != null) {
 							return result;
 						}
 					}
-					
-					
+
 					throw new RuntimeException("No value for " + name + " declared in the annotation " + clazz);
 				}
 			});
-			
+
 			return result;
-			
-			
+
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException("Could not load class: " + tag.getType());
 		}
