c6c01a1f27a438b5716fbcd768665c5aee486910
Steven Arzt <Steven.Arzt@cased.de>
2015-05-06 15:02:35 +0200

    optimized the implementation slightly

--- src/soot/toolkits/graph/StronglyConnectedComponentsFast.java
+++ src/soot/toolkits/graph/StronglyConnectedComponentsFast.java
@@ -18,110 +18,112 @@
  */
 
 package soot.toolkits.graph;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
 /**
- *  Identifies and provides an interface to query the strongly-connected
- *  components of DirectedGraph instances.
- *  
- *  Uses Tarjan's algorithm.
- *  
- *  @see DirectedGraph
- *  @author Eric Bodden
+ * Identifies and provides an interface to query the strongly-connected
+ * components of DirectedGraph instances.
+ * 
+ * Uses Tarjan's algorithm.
+ * 
+ * @see DirectedGraph
+ * @author Eric Bodden
  */
 
-public class StronglyConnectedComponentsFast<N>
-{
-  protected final List<List<N>> componentList = new ArrayList<List<N>>();
-  protected final List<List<N>> trueComponentList = new ArrayList<List<N>>();
+public class StronglyConnectedComponentsFast<N> {
+	protected final List<List<N>> componentList = new ArrayList<List<N>>();
+	protected final List<List<N>> trueComponentList = new ArrayList<List<N>>();
 
-  protected int index = 0;
+	protected int index = 0;
 
-  protected Map<N,Integer> indexForNode, lowlinkForNode;
+	protected Map<N, Integer> indexForNode, lowlinkForNode;
 
-  protected Stack<N> s;
+	protected Stack<N> s;
 
-  protected DirectedGraph<N> g;
-    
-  /**
-   *  @param g a graph for which we want to compute the strongly
-   *           connected components. 
-   *  @see DirectedGraph
-   */
-  public StronglyConnectedComponentsFast(DirectedGraph<N> g)
-  {
-    this.g = g;
-    s = new Stack<N>();
-    List<N> heads = g.getHeads();
+	protected DirectedGraph<N> g;
 
-    indexForNode = new HashMap<N, Integer>();
-    lowlinkForNode = new HashMap<N, Integer>();
+	/**
+	 * @param g
+	 *            a graph for which we want to compute the strongly connected
+	 *            components.
+	 * @see DirectedGraph
+	 */
+	public StronglyConnectedComponentsFast(DirectedGraph<N> g) {
+		this.g = g;
+		s = new Stack<N>();
+		List<N> heads = g.getHeads();
 
-    for(Iterator<N> headsIt = heads.iterator(); headsIt.hasNext(); ) {
-      N head = headsIt.next();
-      if(!indexForNode.containsKey(head)) {
-        recurse(head);
-      }
-    }
+		indexForNode = new HashMap<N, Integer>();
+		lowlinkForNode = new HashMap<N, Integer>();
 
-    //free memory
-    indexForNode = null;
-    lowlinkForNode = null;
-    s = null;
-    g = null;
-  }
+		for (N head : heads) {
+			if (!indexForNode.containsKey(head)) {
+				recurse(head);
+			}
+		}
 
-  protected void recurse(N v) {
-    indexForNode.put(v, index);
-    lowlinkForNode.put(v, index);
-    index++;
-    s.push(v);
-    for(N succ: g.getSuccsOf(v)) {
-      if(!indexForNode.containsKey(succ)) {
-        recurse(succ);
-        lowlinkForNode.put(v, Math.min(lowlinkForNode.get(v), lowlinkForNode.get(succ)));
-      } else if(s.contains(succ)) {
-        lowlinkForNode.put(v, Math.min(lowlinkForNode.get(v), indexForNode.get(succ)));
-      }			
-    }
-    if(lowlinkForNode.get(v).intValue() == indexForNode.get(v).intValue()) {
-      List<N> scc = new ArrayList<N>();
-      N v2;
-      do {
-        v2 = s.pop();
-        scc.add(v2);
-      }while(v!=v2);			
-      componentList.add(scc);
-      if(scc.size()>1) {
-        trueComponentList.add(scc);
-      } else {
-        N n = scc.get(0);
-        if(g.getSuccsOf(n).contains(n))
-          trueComponentList.add(scc);
-      }
-    }
-  }
+		// free memory
+		indexForNode = null;
+		lowlinkForNode = null;
+		s = null;
+		g = null;
+	}
 
-  /**
-   *   @return the list of the strongly-connected components
-   */
-  public List<List<N>> getComponents()
-  {
-    return componentList;
-  }
+	protected void recurse(N v) {
+		int lowLinkForNodeV;
+		indexForNode.put(v, index);
+		lowlinkForNode.put(v, lowLinkForNodeV = index);
+		index++;
+		s.push(v);
 
-  /**
-   *   @return the list of the strongly-connected components, but only those
-   *   that are true components, i.e. components which have more than one element
-   *   or consists of one node that has itself as a successor 
-   */
-  public List<List<N>> getTrueComponents()
-  {
-    return trueComponentList;
-  }
+		for (N succ : g.getSuccsOf(v)) {
+			Integer indexForNodeSucc = indexForNode.get(succ);
+			if (indexForNodeSucc == null) {
+				recurse(succ);
+				lowlinkForNode.put(v, lowLinkForNodeV = Math.min(lowLinkForNodeV,
+						lowlinkForNode.get(succ)));
+			} else if (s.contains(succ)) {
+				lowlinkForNode.put(v, lowLinkForNodeV = Math.min(lowLinkForNodeV,
+						indexForNodeSucc));
+			}
+		}
+		if (lowLinkForNodeV == indexForNode.get(v).intValue()) {
+			List<N> scc = new ArrayList<N>();
+			N v2;
+			do {
+				v2 = s.pop();
+				scc.add(v2);
+			} while (v != v2);
+			componentList.add(scc);
+			if (scc.size() > 1) {
+				trueComponentList.add(scc);
+			} else {
+				N n = scc.get(0);
+				if (g.getSuccsOf(n).contains(n))
+					trueComponentList.add(scc);
+			}
+		}
+	}
+
+	/**
+	 * @return the list of the strongly-connected components
+	 */
+	public List<List<N>> getComponents() {
+		return componentList;
+	}
+
+	/**
+	 * @return the list of the strongly-connected components, but only those
+	 *         that are true components, i.e. components which have more than
+	 *         one element or consists of one node that has itself as a
+	 *         successor
+	 */
+	public List<List<N>> getTrueComponents() {
+		return trueComponentList;
+	}
 }
