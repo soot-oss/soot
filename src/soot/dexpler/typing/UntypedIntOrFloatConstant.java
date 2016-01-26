31cf2a2ada53cac3d6a1a6d15a82041c4181ad8c
Alexandre Bartel <alexandre.bartel@cased.de>
2016-01-26 12:07:25 +0100

    Added defineType abstract method. Depending on context this method should return the appropriate type for the constant

--- src/soot/dexpler/typing/UntypedIntOrFloatConstant.java
+++ src/soot/dexpler/typing/UntypedIntOrFloatConstant.java
@@ -1,7 +1,17 @@
 package soot.dexpler.typing;
 
+import soot.BooleanType;
+import soot.ByteType;
+import soot.CharType;
+import soot.FloatType;
+import soot.IntType;
+import soot.RefLikeType;
+import soot.ShortType;
+import soot.Type;
+import soot.Value;
 import soot.jimple.FloatConstant;
 import soot.jimple.IntConstant;
+import soot.jimple.NullConstant;
 
 public class UntypedIntOrFloatConstant extends UntypedConstant {
 
@@ -39,5 +49,21 @@ public class UntypedIntOrFloatConstant extends UntypedConstant {
     public IntConstant toIntConstant() {
         return IntConstant.v(value);
     }
+
+	@Override
+	public Value defineType(Type t) {
+		if (t instanceof FloatType) {
+			return this.toFloatConstant();
+		} else if (t instanceof IntType || t instanceof CharType || t instanceof BooleanType || t instanceof ByteType || t instanceof ShortType) {
+			return this.toIntConstant();
+		} else {
+			if (value == 0 && t instanceof RefLikeType)
+				return NullConstant.v();
+			if (t == null) { // if the value is only used in a if to compare against another integer, then use default type of integer
+				return this.toIntConstant();
+			}
+			throw new RuntimeException("error: expected Float type or Int-like type. Got " + t);
+		}
+	}
 
 }
