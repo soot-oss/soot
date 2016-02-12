d27ca5ba78cbd48e4933278dfe9290343ac2181f
Steven Arzt <Steven.Arzt@cased.de>
2016-02-12 17:07:12 +0100

    also perform copy propagation by default

--- src/soot/PackManager.java
+++ src/soot/PackManager.java
@@ -970,6 +970,7 @@ public class PackManager {
             if (produceJimple) {
                 Body body = m.retrieveActiveBody();
                 //Change
+                CopyPropagator.v().transform(body);
                 ConditionalBranchFolder.v().transform(body);
                 UnreachableCodeEliminator.v().transform(body);
                 DeadAssignmentEliminator.v().transform(body);
