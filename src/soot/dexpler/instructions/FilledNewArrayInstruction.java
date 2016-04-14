ecebeb6698a03bb3d73f36cf1bd722807acaa652
Steven Arzt <Steven.Arzt@cased.de>
2016-04-14 12:18:43 +0200

    removed debug output

--- src/soot/dexpler/instructions/FilledNewArrayInstruction.java
+++ src/soot/dexpler/instructions/FilledNewArrayInstruction.java
@@ -70,7 +70,6 @@ public class FilledNewArrayInstruction extends FilledArrayInstruction {
         Type t = DexType.toSoot((TypeReference) filledNewArrayInstr.getReference());
         // NewArrayExpr needs the ElementType as it increases the array dimension by 1
         Type arrayType = ((ArrayType) t).getElementType();
-System.out.println("array element type: (filled narr)"+ arrayType);
         NewArrayExpr arrayExpr = Jimple.v().newNewArrayExpr(arrayType, IntConstant.v(usedRegister));
         // new local generated intentional, will be moved to real register by MoveResult
         arrayLocal = body.getStoreResultLocal();
