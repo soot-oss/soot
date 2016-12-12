a8217b98a73c9f0491c377af9dd23d3dd0ed15e3
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2016-12-12 17:54:24 +0100

    rewrote the code that finds all statements that have a monitor on them. The new implementation now supports nested monitors

--- src/soot/toolkits/exceptions/TrapTransformer.java
+++ src/soot/toolkits/exceptions/TrapTransformer.java
@@ -2,15 +2,17 @@ package soot.toolkits.exceptions;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import soot.BodyTransformer;
+import soot.Local;
 import soot.Unit;
 import soot.jimple.EnterMonitorStmt;
 import soot.jimple.ExitMonitorStmt;
 import soot.toolkits.graph.UnitGraph;
+import soot.util.HashMultiMap;
+import soot.util.MultiMap;
 
 /**
  * Common abstract base class for all body transformers that change the trap
@@ -20,43 +22,50 @@ import soot.toolkits.graph.UnitGraph;
  *
  */
 public abstract class TrapTransformer extends BodyTransformer {
-
+	
 	public Set<Unit> getUnitsWithMonitor(UnitGraph ug) {
-		Set<Unit> unitsWithMonitor = new HashSet<Unit>();
+		// Idea: Associate each unit with a set of monitors held at that
+		// statement
+		MultiMap<Unit, Local> unitMonitors = new HashMultiMap<>();
 		
+		// Start at the heads of the unit graph
+		List<Unit> workList = new ArrayList<>();
+		Set<Unit> doneSet = new HashSet<>();
 		for (Unit head : ug.getHeads()) {
-			List<Unit> workList = new ArrayList<Unit>();
 			workList.add(head);
-			Set<Unit> doneSet = new HashSet<Unit>();
-			doneSet.add(head);
-			
-			while (!workList.isEmpty()) {
-				Unit curUnit = workList.remove(0);				
-				for (Unit succ : ug.getSuccsOf(curUnit))
-					if (doneSet.add(succ))
-						workList.add(succ);
-				
-				// If this is an "entermonitor" construct,
-				// we're in a monitor from now on
-				if (curUnit instanceof EnterMonitorStmt)
-					unitsWithMonitor.add(curUnit);
-				// If we leave a monitor, we're out now
-				else if (curUnit instanceof ExitMonitorStmt)
-					continue;
-				else {
-					for (Unit pred : ug.getPredsOf(curUnit))
-						if (unitsWithMonitor.contains(pred))
-							unitsWithMonitor.add(curUnit);
-				}
-			}
 		}
 		
-		// Get rid of the entermonitor statements themselves
-		for (Iterator<Unit> it = unitsWithMonitor.iterator(); it.hasNext(); )
-			if (it.next() instanceof EnterMonitorStmt)
-				it.remove();
+		while (!workList.isEmpty()) {
+			Unit curUnit = workList.remove(0);
+			
+			boolean hasChanged = false;
+			Local exitLocal = null;
+			if (curUnit instanceof EnterMonitorStmt) {
+				// We enter a new monitor
+				EnterMonitorStmt ems = (EnterMonitorStmt) curUnit;
+				if (ems.getOp() instanceof Local)
+					hasChanged = unitMonitors.put(curUnit, (Local) ems.getOp());
+			}
+			else if (curUnit instanceof ExitMonitorStmt) {
+				// We leave a minitor
+				ExitMonitorStmt ems = (ExitMonitorStmt) curUnit;
+				if (ems.getOp() instanceof Local)
+					exitLocal = (Local) ems.getOp();
+			}
+			
+			// Copy over the monitors from the predecessors
+			for (Unit pred : ug.getPredsOf(curUnit))
+				for (Local l : unitMonitors.get(pred))
+					if (l != exitLocal)
+						if (unitMonitors.put(curUnit, l))
+							hasChanged = true;
+			
+			// Work on the successors
+			if (doneSet.add(curUnit) || hasChanged)
+				workList.addAll(ug.getSuccsOf(curUnit));
+		}
 		
-		return unitsWithMonitor;
+		return unitMonitors.keySet();
 	}
 
 }
