81c2b62ab014fdb333eb8d2c0afb76177b56e90a
Manuel Benz <mbenz89@users.noreply.github.com>
2017-04-24 12:25:56 +0200

    Backport of StringNumberer to Java 7
    Soot targets Java 7 while map.pufIfAbsent() is a Jdk8 method.

--- src/soot/util/StringNumberer.java
+++ src/soot/util/StringNumberer.java
@@ -34,12 +34,11 @@ public class StringNumberer extends ArrayNumberer<NumberedString> {
     		new HashMap<String, NumberedString>(1024);
     
     public synchronized NumberedString findOrAdd( String s ) {
-    	NumberedString numStr = new NumberedString(s);
-        NumberedString ret = stringToNumbered.putIfAbsent(s, numStr);
-        if( ret == null ) {
-            add(numStr);
-            return numStr;
-        }
-        return ret;
+            NumberedString ret = stringToNumbered.get(s);
+            if( ret == null ) {
+                ret = stringToNumbered.put(s, new NumberedString(s));
+                add(ret);
+            }
+            return ret;
     }
 }
