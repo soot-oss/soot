8969dbb23930cb340275d34554ff7c07b67d2ce6
Manuel Benz <manuel.benz@upb.de>
2017-09-25 16:28:01 +0200

    Types of reference items generalized to "Reference"-interface.

--- src/soot/toDex/instructions/Insn21c.java
+++ src/soot/toDex/instructions/Insn21c.java
@@ -5,8 +5,11 @@ import java.util.BitSet;
 import org.jf.dexlib2.Opcode;
 import org.jf.dexlib2.builder.BuilderInstruction;
 import org.jf.dexlib2.builder.instruction.BuilderInstruction21c;
+import org.jf.dexlib2.iface.reference.FieldReference;
+import org.jf.dexlib2.iface.reference.Reference;
 import org.jf.dexlib2.writer.builder.BuilderReference;
 
+import soot.jimple.FieldRef;
 import soot.toDex.LabelAssigner;
 import soot.toDex.Register;
 
@@ -18,9 +21,9 @@ import soot.toDex.Register;
  */
 public class Insn21c extends AbstractInsn implements OneRegInsn {
 	
-	private BuilderReference referencedItem;
+	private Reference referencedItem;
 
-	public Insn21c(Opcode opc, Register regA, BuilderReference referencedItem) {
+	public Insn21c(Opcode opc, Register regA, Reference referencedItem) {
 		super(opc);
 		regs.add(regA);
 		this.referencedItem = referencedItem;
