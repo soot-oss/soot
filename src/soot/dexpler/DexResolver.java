832715bd350eb6dbf9a17452069e758e0650ddc9
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2017-09-26 19:15:44 +0200

    refactorings to improve extensibility

--- src/soot/dexpler/DexResolver.java
+++ src/soot/dexpler/DexResolver.java
@@ -35,47 +35,63 @@ import soot.javaToJimple.IInitialResolver.Dependencies;
 
 public class DexResolver {
 
-	private Map<File,DexlibWrapper> cache = new TreeMap<File, DexlibWrapper>();
+	protected Map<File, DexlibWrapper> cache = new TreeMap<File, DexlibWrapper>();
 
-    public DexResolver(Singletons.Global g) {}
+	public DexResolver(Singletons.Global g) {
+	}
 
-    public static DexResolver v() { return G.v().soot_dexpler_DexResolver(); }
+	public static DexResolver v() {
+		return G.v().soot_dexpler_DexResolver();
+	}
 
-    /**
-     * Resolve the class contained in file into the passed soot class.
-     *
-     * @param file the path to the dex/apk file to resolve
-     * @param className the name of the class to resolve
-     * @param sc the soot class that will represent the class
-     * @return the dependencies of this class.
-     */
-    public Dependencies resolveFromFile(File file, String className, SootClass sc) {
-    	DexlibWrapper wrapper = cache.get(file);
-    	if(wrapper==null) {
-    		wrapper = new DexlibWrapper(file);
-    		cache.put(file, wrapper);
-    		wrapper.initialize();
-    	}
+	/**
+	 * Resolve the class contained in file into the passed soot class.
+	 *
+	 * @param file
+	 *            the path to the dex/apk file to resolve
+	 * @param className
+	 *            the name of the class to resolve
+	 * @param sc
+	 *            the soot class that will represent the class
+	 * @return the dependencies of this class.
+	 */
+	public Dependencies resolveFromFile(File file, String className, SootClass sc) {
+		DexlibWrapper wrapper = initializeDexFile(file);
+		Dependencies deps = wrapper.makeSootClass(sc, className);
+		addSourceFileTag(sc, "dalvik_source_" + file.getName());
 
-        Dependencies deps = wrapper.makeSootClass(sc, className);
-        addSourceFileTag(sc, "dalvik_source_" + file.getName());
+		return deps;
+	}
 
-        return deps;
-    }
+	/**
+	 * Initializes the dex wrapper for the given dex file
+	 * 
+	 * @param file
+	 *            The dex file to load
+	 * @return The wrapper object for the given dex file
+	 */
+	protected DexlibWrapper initializeDexFile(File file) {
+		DexlibWrapper wrapper = cache.get(file);
+		if (wrapper == null) {
+			wrapper = new DexlibWrapper(file);
+			cache.put(file, wrapper);
+			wrapper.initialize();
+		}
+		return wrapper;
+	}
 
-    /**
-     *  adds source file tag to each sootclass
-     */
-    protected static void addSourceFileTag(SootClass sc, String fileName){
-        soot.tagkit.SourceFileTag tag = null;
-        if (sc.hasTag("SourceFileTag")) {
-            return; // do not add tag if original class already has debug
-                    // information
-        }
-        else {
-            tag = new soot.tagkit.SourceFileTag();
-            sc.addTag(tag);
-        }
-        tag.setSourceFile(fileName);
-    }
+	/**
+	 * adds source file tag to each sootclass
+	 */
+	protected static void addSourceFileTag(SootClass sc, String fileName) {
+		soot.tagkit.SourceFileTag tag = null;
+		if (sc.hasTag("SourceFileTag")) {
+			return; // do not add tag if original class already has debug
+					// information
+		} else {
+			tag = new soot.tagkit.SourceFileTag();
+			sc.addTag(tag);
+		}
+		tag.setSourceFile(fileName);
+	}
 }
