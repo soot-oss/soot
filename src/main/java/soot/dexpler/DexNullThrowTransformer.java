bfc5a72dcb9baad757543fe1a8fd757497aed12f
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2018-02-13 20:44:10 +0100

    constructors must be called using specialinvoke expressions

--- src/main/java/soot/dexpler/DexNullThrowTransformer.java
+++ src/main/java/soot/dexpler/DexNullThrowTransformer.java
@@ -24,7 +24,7 @@ import soot.jimple.toolkits.scalar.LocalCreation;
 /**
  * Some Android applications throw null references, e.g.,
  * 
- * 		a = null; throw a;
+ * a = null; throw a;
  * 
  * This will make unit graph construction fail as no targets of the throw
  * statement can be found. We therefore replace such statements with direct
@@ -34,23 +34,22 @@ import soot.jimple.toolkits.scalar.LocalCreation;
  * 
  */
 public class DexNullThrowTransformer extends BodyTransformer {
-	
+
 	public static DexNullThrowTransformer v() {
 		return new DexNullThrowTransformer();
 	}
-	
+
 	@Override
 	protected void internalTransform(Body b, String phaseName, Map<String, String> options) {
 		LocalCreation lc = new LocalCreation(b.getLocals(), "ex");
-		
-		for (Iterator<Unit> unitIt = b.getUnits().snapshotIterator(); unitIt.hasNext(); ) {
+
+		for (Iterator<Unit> unitIt = b.getUnits().snapshotIterator(); unitIt.hasNext();) {
 			Unit u = unitIt.next();
-			
+
 			// Check for a null exception
 			if (u instanceof ThrowStmt) {
 				ThrowStmt throwStmt = (ThrowStmt) u;
-				if (throwStmt.getOp() == NullConstant.v()
-						|| throwStmt.getOp().equals(IntConstant.v(0))
+				if (throwStmt.getOp() == NullConstant.v() || throwStmt.getOp().equals(IntConstant.v(0))
 						|| throwStmt.getOp().equals(LongConstant.v(0))) {
 					createThrowStmt(b, throwStmt, lc);
 				}
@@ -60,28 +59,31 @@ public class DexNullThrowTransformer extends BodyTransformer {
 
 	/**
 	 * Creates a new statement that throws a NullPointerException
-	 * @param body The body in which to create the statement
-	 * @param oldStmt The old faulty statement that shall be replaced with the
-	 * exception
-	 * @param lc The object for creating new locals
+	 * 
+	 * @param body
+	 *            The body in which to create the statement
+	 * @param oldStmt
+	 *            The old faulty statement that shall be replaced with the
+	 *            exception
+	 * @param lc
+	 *            The object for creating new locals
 	 */
 	private void createThrowStmt(Body body, Unit oldStmt, LocalCreation lc) {
 		RefType tp = RefType.v("java.lang.NullPointerException");
 		Local lcEx = lc.newLocal(tp);
-		
+
 		SootMethodRef constructorRef = Scene.v().makeConstructorRef(tp.getSootClass(),
 				Collections.singletonList((Type) RefType.v("java.lang.String")));
-		
+
 		// Create the exception instance
 		Stmt newExStmt = Jimple.v().newAssignStmt(lcEx, Jimple.v().newNewExpr(tp));
 		body.getUnits().insertBefore(newExStmt, oldStmt);
-		Stmt invConsStmt = Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(lcEx,
-				constructorRef, Collections.singletonList(StringConstant.v(
-						"Null throw statement replaced by Soot"))));
+		Stmt invConsStmt = Jimple.v().newInvokeStmt(Jimple.v().newSpecialInvokeExpr(lcEx, constructorRef,
+				Collections.singletonList(StringConstant.v("Null throw statement replaced by Soot"))));
 		body.getUnits().insertBefore(invConsStmt, oldStmt);
-		
+
 		// Throw the exception
 		body.getUnits().swapWith(oldStmt, Jimple.v().newThrowStmt(lcEx));
 	}
-	
+
 }
