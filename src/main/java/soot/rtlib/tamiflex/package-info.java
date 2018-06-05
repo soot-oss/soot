111250dc8416c7e11af1e9c949acbba76bd0af61
Manuel Benz <manuelf.benz@gmail.com>
2018-06-05 18:27:30 +0200

    Fixed messed-up package info

--- src/main/java/soot/rtlib/tamiflex/package-info.java
+++ src/main/java/soot/rtlib/tamiflex/package-info.java
@@ -1,14 +1,3 @@
-/**
- * This package contains classes that may be emitted into a program during code generation. If Soot cannot find a class on
- * the soot-classpath then it automatically loads the class from Soot's own JAR file but <i>only</i> if this class is in this
- * package. This is to avoid accidental mix-ups between the classes of the application being analyzed and Soot's own classes.
- *
- * To add a class, use, for example, the following:
- *
- * <pre>
- * // before calling soot.Main.main
- * Scene.v().addBasicClass(SootSig.class.getName(), SootClass.BODIES);
-
 /*-
  * #%L
  * Soot - a J*va Optimization Framework
@@ -30,7 +19,17 @@
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
+ 
+/**
+ * This package contains classes that may be emitted into a program during code generation. If Soot cannot find a class on
+ * the soot-classpath then it automatically loads the class from Soot's own JAR file but <i>only</i> if this class is in this
+ * package. This is to avoid accidental mix-ups between the classes of the application being analyzed and Soot's own classes.
  *
+ * To add a class, use, for example, the following:
+ *
+ * <pre>
+ * // before calling soot.Main.main
+ * Scene.v().addBasicClass(SootSig.class.getName(), SootClass.BODIES);
  * // then at some point
  * Scene.v().getSootClass(SootSig.class.getName()).setApplicationClass();
  * </pre>
