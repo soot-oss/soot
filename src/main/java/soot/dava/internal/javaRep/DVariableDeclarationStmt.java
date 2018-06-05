b281482970525a5ec24d9199c127e98a82be0a03
Manuel Benz <manuelf.benz@gmail.com>
2018-06-05 18:17:46 +0200

    Script based header convertion

--- src/main/java/soot/dava/internal/javaRep/DVariableDeclarationStmt.java
+++ src/main/java/soot/dava/internal/javaRep/DVariableDeclarationStmt.java
@@ -1,38 +1,26 @@
-/* Soot - a J*va Optimization Framework
- * Copyright (C) 2005 Nomair A. Naeem
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
-/*
- *TODO: Nomair (November 23rd 2005) should look at all methods in AbstractUnit 
- *                                  and figure out what to do with them!!
- *      With the changed semantics maybe another stmt which deals with int i=2; separatly should be
- *      created
- */
-
-/*
- *  CHANGE LOG:  November 23rd 2005: Changing the semantics of DVariableDeclarationStmt. 
- *                   The fact that we can have definition stmts inside the list of locals
- *                   declared is going to lead to way too many problems. So DVairableDeclarationStmt
- *                   is going to be restricted to declaration which do not have definitions
- *            e.g. int i,j,k; is allowed
- *                  int i=2,j; is not allowed
- */
 package soot.dava.internal.javaRep;
+
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2005 Nomair A. Naeem
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- src/test/java/soot/toolkits/graph/TestDominance.java
+++ src/test/java/soot/toolkits/graph/TestDominance.java
@@ -1,29 +1,27 @@
 package soot.toolkits.graph;
 
-/* Tim Henderson (tadh@case.edu)
- *
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
  * Copyright (c) 2014, Tim Henderson, Case Western Reserve University
  *   Cleveland, Ohio 44106
  *   All Rights Reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor,
- *   Boston, MA  02110-1301
- *   USA
- * or retrieve version 2.1 at their website:
- *   http://www.gnu.org/licenses/lgpl-2.1.html
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
  */
 
 import static org.hamcrest.MatcherAssert.assertThat;
