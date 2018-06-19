1ab532b6eef59061d1501a975169a21f83dbcd1e
John Toman <jtoman@cs.washington.edu>
2018-06-18 21:52:47 -0700

    Supports AnySubType for reaching types
    Our previous implementation only worked on concrete reference types.
    Under Spark this was a MOSTLY a fine assumption, except that the native
    simulators handlers could still introduce anysubtype types into the PAG.
    
    This commit correctly handles this and, in principle, opens the
    door for types-for-invoke to be used with the CHA builder.
    However, the machinery to plug into the CHA is not written (yet).

---
+++ src/systemTest/targets/soot/jimple/toolkit/callgraph/Interface.java
@@ -0,0 +1,27 @@
+package soot.jimple.toolkit.callgraph;
+
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2018 John Toman
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+public interface Interface {
+	public Object[] args();
+}
