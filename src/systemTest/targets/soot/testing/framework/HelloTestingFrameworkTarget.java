70d685d9fa2c390941cb9ff14b6b1cbc90c37b3b
Manuel Benz <manuelf.benz@gmail.com>
2018-06-22 16:18:45 +0200

    Implemented small framework for system testing Soot, i.e., to-be-analyzed java code can be placed into src/systemTest/target and tJunit Tests that analyze these targets can be placed in src/systemTest/java. Also, a simple super class for such cases is provided which helps with generating a call graph for the given targets

---
+++ src/systemTest/targets/soot/testing/framework/HelloTestingFrameworkTarget.java
@@ -0,0 +1,11 @@
+package soot.testing.framework;
+
+/**
+ * @author Manuel Benz created on 22.06.18
+ */
+public class HelloTestingFrameworkTarget {
+
+  public void helloWorld() {
+    System.out.println("Hello testing world!");
+  }
+}
