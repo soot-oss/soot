084b555a4b03617eaf3a5868c9c686082d62b80e
Manuel Benz <manuelf.benz@gmail.com>
2018-06-22 16:53:32 +0200

    Adds missing license headers and license header check for system tests/targets.

--- src/systemTest/java/soot/testing/framework/AbstractTestingFramework.java
+++ src/systemTest/java/soot/testing/framework/AbstractTestingFramework.java
@@ -1,5 +1,27 @@
 package soot.testing.framework;
 
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2018 Manuel Benz
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,7 +49,7 @@ import soot.jimple.NullConstant;
 import soot.options.Options;
 
 /**
- * @author Manuel Benz created on 01.09.17
+ * @author Manuel Benz created on 22.06.18
  */
 @RunWith(PowerMockRunner.class)
 public abstract class AbstractTestingFramework {
