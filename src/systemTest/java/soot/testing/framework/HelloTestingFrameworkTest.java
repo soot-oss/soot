a4cb22f5451b1aaaafb51469fd7f2097b75d1489
Manuel Benz <manuelf.benz@gmail.com>
2018-06-22 20:33:35 +0200

    Made test case a bit more expressive by also checking if the received target method was indeed resolved correctly.

--- src/systemTest/java/soot/testing/framework/HelloTestingFrameworkTest.java
+++ src/systemTest/java/soot/testing/framework/HelloTestingFrameworkTest.java
@@ -38,5 +38,7 @@ public class HelloTestingFrameworkTest extends AbstractTestingFramework {
   public void findsTarget() {
     final SootMethod sootMethod = prepareTarget("<" + TEST_TARGET_CLASS + ": void helloWorld()>", TEST_TARGET_CLASS);
     Assert.assertNotNull("Could not find target method. System test setup seems to be incorrect.", sootMethod);
+    Assert.assertTrue(sootMethod.isConcrete());
+    Assert.assertNotNull(sootMethod.retrieveActiveBody());
   }
 }
