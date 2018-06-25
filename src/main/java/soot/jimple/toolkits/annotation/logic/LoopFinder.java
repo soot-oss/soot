79d7c225d6554955ee4f37f046647ee6bcd2a2ed
Manuel Benz <manuelf.benz@gmail.com>
2018-06-25 14:29:50 +0200

    Fixed formatting

--- src/main/java/soot/jimple/toolkits/annotation/logic/LoopFinder.java
+++ src/main/java/soot/jimple/toolkits/annotation/logic/LoopFinder.java
@@ -42,91 +42,91 @@ import soot.toolkits.graph.UnitGraph;
 public class LoopFinder extends BodyTransformer {
 
   private Set<Loop> loops;
-  
+
   public LoopFinder() {
     loops = null;
   }
-  
-  protected void internalTransform (Body b, String phaseName, Map<String,String> options) {
+
+  protected void internalTransform(Body b, String phaseName, Map<String, String> options) {
     getLoops(b);
   }
-  
+
   public Set<Loop> getLoops(Body b) {
     if (loops != null) {
       return loops;
     }
     return getLoops(new ExceptionalUnitGraph(b));
   }
-  
+
   public Set<Loop> getLoops(UnitGraph g) {
     if (loops != null) {
       return loops;
     }
-  
+
     MHGDominatorsFinder<Unit> a = new MHGDominatorsFinder<Unit>(g);
     Map<Stmt, List<Stmt>> loops = new HashMap<Stmt, List<Stmt>>();
-    
-    for(Unit u : g.getBody().getUnits()) {
+
+    for (Unit u : g.getBody().getUnits()) {
       List<Unit> succs = g.getSuccsOf(u);
       List<Unit> dominaters = a.getDominators(u);
       List<Stmt> headers = new ArrayList<Stmt>();
-      
-      for(Unit succ : succs) {
+
+      for (Unit succ : succs) {
         if (dominaters.contains(succ)) {
-          //header succeeds and dominates s, we have a loop
-          headers.add((Stmt)succ);
+          // header succeeds and dominates s, we have a loop
+          headers.add((Stmt) succ);
         }
       }
-      
-      for(Unit header : headers) {
+
+      for (Unit header : headers) {
         List<Stmt> loopBody = getLoopBodyFor(header, u, g);
         if (loops.containsKey(header)) {
           // merge bodies
           List<Stmt> lb1 = loops.get(header);
-          loops.put((Stmt)header, union(lb1, loopBody));
+          loops.put((Stmt) header, union(lb1, loopBody));
         } else {
-          loops.put((Stmt)header, loopBody);
+          loops.put((Stmt) header, loopBody);
         }
       }
     }
-    
+
     Set<Loop> ret = new HashSet<Loop>();
-    for (Map.Entry<Stmt,List<Stmt>> entry : loops.entrySet()) {
-      ret.add(new Loop(entry.getKey(),entry.getValue(),g));
+    for (Map.Entry<Stmt, List<Stmt>> entry : loops.entrySet()) {
+      ret.add(new Loop(entry.getKey(), entry.getValue(), g));
     }
-    
+
     this.loops = ret;
     return ret;
   }
-  
+
   private List<Stmt> getLoopBodyFor(Unit header, Unit node, UnitGraph g) {
     List<Stmt> loopBody = new ArrayList<Stmt>();
     Deque<Unit> stack = new ArrayDeque<Unit>();
-    
-    loopBody.add((Stmt)header);
+
+    loopBody.add((Stmt) header);
     stack.push(node);
-    
+
     while (!stack.isEmpty()) {
-      Stmt next = (Stmt)stack.pop();
+      Stmt next = (Stmt) stack.pop();
       if (!loopBody.contains(next)) {
         // add next to loop body
         loopBody.add(0, next);
         // put all preds of next on stack
-        for(Unit u : g.getPredsOf(next)) {
+        for (Unit u : g.getPredsOf(next)) {
           stack.push(u);
         }
       }
     }
-    
-    assert (node==header && loopBody.size()==1) || loopBody.get(loopBody.size()-2)==node;
-    assert loopBody.get(loopBody.size()-1)==header;
-    
+
+    assert (node == header && loopBody.size() == 1) || loopBody.get(loopBody.size() - 2) == node;
+    assert loopBody.get(loopBody.size() - 1) == header;
+
     return loopBody;
   }
-  
+
   private List<Stmt> union(List<Stmt> l1, List<Stmt> l2) {
-    for(Stmt next : l2) {
-      if (!l1.contains(next)){
+    for (Stmt next : l2) {
+      if (!l1.contains(next)) {
         l1.add(next);
       }
     }
