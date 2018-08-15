30ae7edd899ff319db5d21496d0f116cfb5bddfa
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2018-08-15 13:45:21 +0200

    Moving insert on edge to patching chain, since it actually implies that the chain is only handling units.

--- src/main/java/soot/PatchingChain.java
+++ src/main/java/soot/PatchingChain.java
@@ -23,11 +23,14 @@ package soot;
  */
 
 import java.util.AbstractCollection;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import soot.jimple.GotoStmt;
+import soot.jimple.Jimple;
 import soot.util.Chain;
 
 /**
@@ -53,31 +56,37 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
   }
 
   /** Adds the given object to this Chain. */
+  @Override
   public boolean add(E o) {
     return innerChain.add(o);
   }
 
   /** Replaces <code>out</code> in the Chain by <code>in</code>. */
+  @Override
   public void swapWith(E out, E in) {
     innerChain.swapWith(out, in);
     out.redirectJumpsToThisTo(in);
   }
 
   /** Inserts <code>toInsert</code> in the Chain after <code>point</code>. */
+  @Override
   public void insertAfter(E toInsert, E point) {
     innerChain.insertAfter(toInsert, point);
   }
 
   /** Inserts <code>toInsert</code> in the Chain after <code>point</code>. */
+  @Override
   public void insertAfter(List<E> toInsert, E point) {
     innerChain.insertAfter(toInsert, point);
   }
 
+  @Override
   public void insertAfter(Chain<E> toInsert, E point) {
     innerChain.insertAfter(toInsert, point);
   }
 
   /** Inserts <code>toInsert</code> in the Chain before <code>point</code>. */
+  @Override
   public void insertBefore(List<E> toInsert, E point) {
     E previousPoint = point;
 
@@ -90,6 +99,7 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
   }
 
   /** Inserts <code>toInsert</code> in the Chain before <code>point</code>. */
+  @Override
   public void insertBefore(Chain<E> toInsert, E point) {
     Object[] obj = toInsert.toArray();
     E previousPoint = point;
@@ -102,6 +112,7 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
   }
 
   /** Inserts <code>toInsert</code> in the Chain before <code>point</code>. */
+  @Override
   public void insertBefore(E toInsert, E point) {
     point.redirectJumpsToThisTo(toInsert);
     innerChain.insertBefore(toInsert, point);
@@ -124,7 +135,136 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
    *          the target point of an edge
    */
   public void insertOnEdge(E toInsert, E point_src, E point_tgt) {
-    innerChain.insertOnEdge(toInsert, point_src, point_tgt);
+    List<E> o = new ArrayList<E>();
+    o.add(toInsert);
+    insertOnEdge(o, point_src, point_tgt);
+
+  }
+
+  /**
+   * Inserts instrumentation in a manner such that the resulting control flow graph (CFG) of the program will contain
+   * <code>toInsert</code> on an edge that is defined by <code>point_source</code> and <code>point_target</code>.
+   *
+   * @param toInsert
+   *          instrumentation to be added in the Chain
+   * @param point_src
+   *          the source point of an edge in CFG
+   * @param point_tgt
+   *          the target point of an edge
+   */
+  public void insertOnEdge(Collection<? extends E> toInsert, E point_src, E point_tgt) {
+
+    if (toInsert == null) {
+      throw new RuntimeException("Bad idea! You tried to insert a null object into a Chain!");
+    }
+
+    // Insert 'toInsert' before 'target' point in chain if the source point
+    // is null
+    if (point_src == null && point_tgt != null) {
+      point_tgt.redirectJumpsToThisTo(toInsert.iterator().next());
+      innerChain.insertBefore(toInsert, point_tgt);
+      return;
+    }
+
+    // Insert 'toInsert' after 'source' point in chain if the target point
+    // is null
+    if (point_src != null && point_tgt == null) {
+      innerChain.insertAfter(toInsert, point_src);
+      return;
+    }
+
+    // Throw an exception if both source and target is null
+    if (point_src == null && point_tgt == null) {
+      throw new RuntimeException("insertOnEdge failed! Both source and target points are null.");
+    }
+
+    // If target is right after the source in the Chain
+    // 1- Redirect all jumps (if any) from 'source' to 'target', to
+    // 'toInsert[0]'
+    // (source->target) ==> (source->toInsert[0])
+    // 2- Insert 'toInsert' after 'source' in Chain
+    if (getSuccOf(point_src) == point_tgt) {
+      List<UnitBox> boxes = point_src.getUnitBoxes();
+      for (UnitBox box : boxes) {
+        if (box.getUnit() == point_tgt) {
+          box.setUnit(toInsert.iterator().next());
+        }
+      }
+      innerChain.insertAfter(toInsert, point_src);
+      return;
+    }
+
+    // If the target is not right after the source in chain then,
+    // 1- Redirect all jumps (if any) from 'source' to 'target', to
+    // 'toInsert[0]'
+    // (source->target) ==> (source->toInsert[0])
+    // 1.1- if there are no jumps from source to target, then such an edge
+    // does not exist. Throw an exception.
+    // 2- Insert 'toInsert' before 'target' in Chain
+    // 3- If required, add a 'goto target' statement so that no other edge
+    // executes 'toInsert'
+    boolean validEdgeFound = false;
+    Unit originalPred = getPredOf(point_tgt);
+
+    List<UnitBox> boxes = point_src.getUnitBoxes();
+    for (UnitBox box : boxes) {
+      if (box.getUnit() == point_tgt) {
+
+        if (point_src instanceof GotoStmt) {
+
+          box.setUnit(toInsert.iterator().next());
+          innerChain.insertAfter(toInsert, point_src);
+
+          Unit goto_unit = Jimple.v().newGotoStmt(point_tgt);
+          if (toInsert instanceof List) {
+            List<Unit> l = (List<Unit>) toInsert;
+            innerChain.insertAfter((E) goto_unit, (E) l.get(l.size() - 1));
+          } else {
+            innerChain.insertAfter((E) goto_unit, (E) toInsert.toArray()[toInsert.size() - 1]);
+          }
+          return;
+        }
+
+        box.setUnit(toInsert.iterator().next());
+
+        validEdgeFound = true;
+      }
+    }
+    if (validEdgeFound) {
+      innerChain.insertBefore(toInsert, point_tgt);
+
+      if (originalPred != point_src) {
+        if (originalPred instanceof GotoStmt) {
+          return;
+        }
+
+        Unit goto_unit = Jimple.v().newGotoStmt(point_tgt);
+        innerChain.insertBefore((List<E>) goto_unit, toInsert.iterator().next());
+      }
+      return;
+    }
+
+    // In certain scenarios, the above code can add extra 'goto' units on a
+    // different edge
+    // So, an edge [src --> tgt] becomes [src -> goto tgt -> tgt].
+    // When this happens, the original edge [src -> tgt] ceases to exist.
+    // The following code handles such scenarios.
+    final Unit succ = getSuccOf(point_src);
+    if (succ instanceof GotoStmt) {
+      if (succ.getUnitBoxes().get(0).getUnit() == point_tgt) {
+
+        getSuccOf(point_src).redirectJumpsToThisTo(toInsert.iterator().next());
+        innerChain.insertBefore(toInsert, getSuccOf(point_src));
+
+        return;
+      }
+    }
+
+    // If the control reaches this point, it means that an edge [src -> tgt]
+    // as specified by user does not exist and is thus invalid
+    // Return an exception.
+    throw new RuntimeException(
+        "insertOnEdge failed! No such edge found. The edge on which you want to insert an instrumentation is invalid.");
   }
 
   /**
@@ -139,7 +279,7 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
    *          the target point of an edge
    */
   public void insertOnEdge(List<E> toInsert, E point_src, E point_tgt) {
-    innerChain.insertOnEdge(toInsert, point_src, point_tgt);
+    insertOnEdge((Collection<E>) toInsert, point_src, point_tgt);
   }
 
   /**
@@ -154,15 +294,17 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
    *          the target point of an edge
    */
   public void insertOnEdge(Chain<E> toInsert, E point_src, E point_tgt) {
-    innerChain.insertOnEdge(toInsert, point_src, point_tgt);
+    insertOnEdge((Collection<E>) toInsert, point_src, point_tgt);
   }
 
   /** Returns true if object <code>a</code> follows object <code>b</code> in the Chain. */
+  @Override
   public boolean follows(E a, E b) {
     return innerChain.follows(a, b);
   }
 
   /** Removes the given object from this Chain. */
+  @Override
   @SuppressWarnings("unchecked")
   public boolean remove(Object obj) {
     boolean res = false;
@@ -192,46 +334,55 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
   }
 
   /** Returns true if this patching chain contains the specified element. */
+  @Override
   public boolean contains(Object u) {
     return innerChain.contains(u);
   }
 
   /** Adds the given object at the beginning of the Chain. */
+  @Override
   public void addFirst(E u) {
     innerChain.addFirst(u);
   }
 
   /** Adds the given object at the end of the Chain. */
+  @Override
   public void addLast(E u) {
     innerChain.addLast(u);
   }
 
   /** Removes the first object from this Chain. */
+  @Override
   public void removeFirst() {
     remove(innerChain.getFirst());
   }
 
   /** Removes the last object from this Chain. */
+  @Override
   public void removeLast() {
     remove(innerChain.getLast());
   }
 
   /** Returns the first object in this Chain. */
+  @Override
   public E getFirst() {
     return innerChain.getFirst();
   }
 
   /** Returns the last object in this Chain. */
+  @Override
   public E getLast() {
     return innerChain.getLast();
   }
 
   /** Returns the object immediately following <code>point</code>. */
+  @Override
   public E getSuccOf(E point) {
     return innerChain.getSuccOf(point);
   }
 
   /** Returns the object immediately preceding <code>point</code>. */
+  @Override
   public E getPredOf(E point) {
     return innerChain.getPredOf(point);
   }
@@ -253,16 +404,19 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
       innerIterator = innerChain.iterator(head, tail);
     }
 
+    @Override
     public boolean hasNext() {
       return innerIterator.hasNext();
     }
 
+    @Override
     public E next() {
       lastObject = innerIterator.next();
       state = true;
       return lastObject;
     }
 
+    @Override
     public void remove() {
       if (!state) {
         throw new IllegalStateException("remove called before first next() call");
@@ -293,27 +447,32 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
    * Returns an iterator over a copy of this chain. This avoids ConcurrentModificationExceptions from being thrown if the
    * underlying Chain is modified during iteration. Do not use this to remove elements which have not yet been iterated over!
    */
+  @Override
   public Iterator<E> snapshotIterator() {
     List<E> l = new LinkedList<E>(this);
     return l.iterator();
   }
 
   /** Returns an iterator over this Chain. */
+  @Override
   public Iterator<E> iterator() {
     return new PatchingIterator(innerChain);
   }
 
   /** Returns an iterator over this Chain, starting at the given object. */
+  @Override
   public Iterator<E> iterator(E u) {
     return new PatchingIterator(innerChain, u);
   }
 
   /** Returns an iterator over this Chain, starting at head and reaching tail (inclusive). */
+  @Override
   public Iterator<E> iterator(E head, E tail) {
     return new PatchingIterator(innerChain, head, tail);
   }
 
   /** Returns the size of this Chain. */
+  @Override
   public int size() {
     return innerChain.size();
   }
@@ -327,5 +486,15 @@ public class PatchingChain<E extends Unit> extends AbstractCollection<E> impleme
   @Override
   public Collection<E> getElementsUnsorted() {
     return innerChain.getElementsUnsorted();
+  }
+
+  @Override
+  public void insertAfter(Collection<? extends E> toInsert, E point) {
+    innerChain.insertAfter(toInsert, point);
+  }
+
+  @Override
+  public void insertBefore(Collection<? extends E> toInsert, E point) {
+    innerChain.insertBefore(toInsert, point);
   }
 }
