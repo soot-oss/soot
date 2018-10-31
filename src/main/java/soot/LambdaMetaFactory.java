5010051b1780d385cb7db68e345e37f928c3d79e
Manuel Benz <manuel.benz@uni-paderborn.de>
2018-10-31 18:07:18 +0100

    Re-implemented bridging logic. The implementation just failed on calls to functional interfaces with generic types.

--- src/main/java/soot/LambdaMetaFactory.java
+++ src/main/java/soot/LambdaMetaFactory.java
@@ -18,6 +18,8 @@
  */
 package soot;
 
+import com.google.common.base.Preconditions;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -25,43 +27,48 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import soot.coffi.Util;
+
+import soot.javaToJimple.LocalGenerator;
 import soot.jimple.ClassConstant;
+import soot.jimple.IdentityStmt;
 import soot.jimple.IntConstant;
 import soot.jimple.Jimple;
 import soot.jimple.JimpleBody;
 import soot.jimple.MethodHandle;
 import soot.jimple.MethodType;
+import soot.jimple.StaticInvokeExpr;
+import soot.jimple.VirtualInvokeExpr;
 
 public final class LambdaMetaFactory {
+  private static final Logger LOGGER = LoggerFactory.getLogger(LambdaMetaFactory.class);
   // FIXME: Move to 'G'?
   private static int uniq;
-  private static final Logger LOGGER = LoggerFactory.getLogger(LambdaMetaFactory.class);
 
   public static SootMethodRef makeLambdaHelper(List<? extends Value> bootstrapArgs, int tag, String name, Type[] types) {
-    if (bootstrapArgs.size() < 3 || !(bootstrapArgs.get(0) instanceof ClassConstant)
-        || !(bootstrapArgs.get(1) instanceof MethodHandle) || !(bootstrapArgs.get(2) instanceof ClassConstant)
+    if (bootstrapArgs.size() < 3 || !(bootstrapArgs.get(0) instanceof MethodType)
+        || !(bootstrapArgs.get(1) instanceof MethodHandle) || !(bootstrapArgs.get(2) instanceof MethodType)
         || (bootstrapArgs.size() > 3 && !(bootstrapArgs.get(3) instanceof IntConstant))) {
       LOGGER.warn("LambdaMetaFactory: unexpected arguments for LambdaMetaFactor.metaFactory: {}", bootstrapArgs);
       return null;
     }
-    String samMethodType = ((ClassConstant) bootstrapArgs.get(0)).getValue();
+
+    MethodType samMethodType = ((MethodType) bootstrapArgs.get(0));
     SootMethodRef implMethod = ((MethodHandle) bootstrapArgs.get(1)).getMethodRef();
-    String instantiatedMethodType = ((ClassConstant) bootstrapArgs.get(2)).getValue();
-    if (!samMethodType.equals(instantiatedMethodType)) {
-      G.v().out.println("warning: LambdaMetaFactory: " + samMethodType + " != " + instantiatedMethodType);
-      return null;
-    }
+    MethodType instantiatedMethodType = ((MethodType) bootstrapArgs.get(2));
+
     int flags = 0;
-    if (bootstrapArgs.size() > 3)
+    if (bootstrapArgs.size() > 3) {
       flags = ((IntConstant) bootstrapArgs.get(3)).value;
+    }
+
     boolean serializable = (flags & 1 /* FLAGS_SERIALIZABLE */) != 0;
     List<ClassConstant> markerInterfaces = new ArrayList<ClassConstant>();
-    List<ClassConstant> bridges = new ArrayList<ClassConstant>();
+    List<MethodType> bridges = new ArrayList<MethodType>();
+
     int va = 4;
     if ((flags & 2 /* FLAG_MARKERS */) != 0) {
       if (va == bootstrapArgs.size() || !(bootstrapArgs.get(va) instanceof IntConstant)) {
-          LOGGER.warn("LambdaMetaFactory: unexpected arguments for LambdaMetaFactory.altMetaFactory");
+        LOGGER.warn("LambdaMetaFactory: unexpected arguments for LambdaMetaFactory.altMetaFactory");
         return null;
       }
       int count = ((IntConstant) bootstrapArgs.get(va++)).value;
@@ -78,6 +85,7 @@ public final class LambdaMetaFactory {
         markerInterfaces.add((ClassConstant) v);
       }
     }
+
     if ((flags & 4 /* FLAG_BRIDGES */) != 0) {
       if (va == bootstrapArgs.size() || !(bootstrapArgs.get(va) instanceof IntConstant)) {
         LOGGER.warn("LambdaMetaFactory: unexpected arguments for LambdaMetaFactory.altMetaFactory");
@@ -90,11 +98,11 @@ public final class LambdaMetaFactory {
           return null;
         }
         Value v = bootstrapArgs.get(va++);
-        if (!(v instanceof ClassConstant)) {
+        if (!(v instanceof MethodType)) {
           LOGGER.warn("LambdaMetaFactory: unexpected arguments for LambdaMetaFactory.altMetaFactory");
           return null;
         }
-        bridges.add((ClassConstant) v);
+        bridges.add((MethodType) v);
       }
     }
 
@@ -107,7 +115,6 @@ public final class LambdaMetaFactory {
 
     // Our thunk class implements the functional interface
     String className = "soot.dummy." + implMethod.name() + "$" + uniqSupply();
-    LOGGER.debug("dummy class name " + className);
     SootClass tclass = new SootClass(className);
     tclass.addInterface(iface);
     if (serializable)
@@ -123,15 +130,14 @@ public final class LambdaMetaFactory {
       tclass.addField(f);
     }
 
-    MethodSource ms;
-    {
-      // FIXME? some duplication from makeBridge... probably should split this into
-      // two different MethodSources
-      Type[] samTypes = Util.v().jimpleTypesOfFieldOrMethodDescriptor(samMethodType);
-      List<Type> paramTypes = Arrays.asList(samTypes).subList(0, samTypes.length - 1);
-      Type retType = samTypes[samTypes.length - 1];
-      ms = new ThunkMethodSource(capFields, paramTypes, retType, implMethod);
-    }
+    List<Type> typedParameterTypes = instantiatedMethodType.getParameterTypes();
+    Type retType = instantiatedMethodType.getReturnType();
+    ThunkMethodSource ms = new ThunkMethodSource(capFields, typedParameterTypes, retType, implMethod);
+
+    // create a method with the concrete, non-erased types first
+    SootMethod apply = new SootMethod(name, typedParameterTypes, retType);
+    tclass.addMethod(apply);
+    apply.setSource(ms);
 
     // Bootstrap method creates a new instance of this class
     SootMethod tboot = new SootMethod("bootstrap$", capTypes, iface.getType(), Modifier.STATIC);
@@ -143,13 +149,16 @@ public final class LambdaMetaFactory {
     tclass.addMethod(tctor);
     tctor.setSource(ms);
 
-    // Dispatch runs the 'real' method implementing the body of the lambda
-    SootMethod tdispatch = makeBridge(name, samMethodType, capFields, implMethod);
-    tclass.addMethod(tdispatch);
+    boolean isGeneric = !samMethodType.equals(instantiatedMethodType);
+    if (isGeneric) {
+      // if the functional interface is generic in it' return-/parameter-types, we have to create bridging method which can
+      // work with plain objects and re-routes the call to the actual thunk method
+      addBridge(name, tclass, samMethodType, instantiatedMethodType, apply);
+    }
 
     for (int i = 0; i < bridges.size(); i++) {
-      SootMethod bridge = makeBridge(name, bridges.get(i).getValue(), capFields, implMethod);
-      tclass.addMethod(bridge);
+      final MethodType bridgeType = bridges.get(i);
+      addBridge(name, tclass, bridgeType, instantiatedMethodType, apply);
     }
 
     Scene.v().addClass(tclass);
@@ -164,17 +173,14 @@ public final class LambdaMetaFactory {
     return tboot.makeRef();
   }
 
-  private static SootMethod makeBridge(String name, String samMethodType, List<SootField> capFields,
-      SootMethodRef implMethod) {
-    Type[] samTypes = Util.v().jimpleTypesOfFieldOrMethodDescriptor(samMethodType);
-    List<Type> paramTypes = Arrays.asList(samTypes).subList(0, samTypes.length - 1);
-    Type retType = samTypes[samTypes.length - 1];
-
-    MethodSource ms = new ThunkMethodSource(capFields, paramTypes, retType, implMethod);
-
-    SootMethod m = new SootMethod(name, paramTypes, retType);
-    m.setSource(ms);
-    return m;
+  private static void addBridge(String name, SootClass tclass, MethodType bridgeType, MethodType targetType,
+      SootMethod apply) {
+    final List<Type> bridgeParams = bridgeType.getParameterTypes();
+    final Type bridgeReturn = bridgeType.getReturnType();
+    SootMethod genericBridge = new SootMethod(name, bridgeParams, bridgeReturn);
+    tclass.addMethod(genericBridge);
+    genericBridge.setSource(new BridgeMethodSource(bridgeParams, targetType.getParameterTypes(), bridgeReturn,
+        targetType.getReturnType(), apply.makeRef()));
   }
 
   private static synchronized long uniqSupply() {
@@ -196,14 +202,15 @@ public final class LambdaMetaFactory {
 
     public Body getBody(SootMethod m, String phaseName) {
       if (!phaseName.equals("jb"))
-        throw new Error("unsupported body type: " + phaseName); // FIXME?
+        throw new Error("unsupported body type: " + phaseName);
 
       SootClass tclass = m.getDeclaringClass();
       JimpleBody jb = Jimple.v().newBody(m);
       PatchingChain<Unit> us = jb.getUnits();
+      LocalGenerator lc = new LocalGenerator(jb);
 
       if (m.getName().equals("<init>")) {
-        Local l = Jimple.v().newLocal("r", tclass.getType());
+        Local l = lc.generateLocal(tclass.getType());
         us.add(Jimple.v().newIdentityStmt(l, Jimple.v().newThisRef(tclass.getType())));
         us.add(Jimple.v()
             .newInvokeStmt(Jimple.v().newSpecialInvokeExpr(l,
@@ -211,43 +218,113 @@ public final class LambdaMetaFactory {
                 Collections.<Value>emptyList())));
         for (SootField f : capFields) {
           int i = us.size() - 2;
-          Local l2 = Jimple.v().newLocal("c" + i, f.getType());
+          Local l2 = lc.generateLocal(f.getType());
           us.add(Jimple.v().newIdentityStmt(l2, Jimple.v().newParameterRef(f.getType(), i)));
           us.add(Jimple.v().newAssignStmt(Jimple.v().newInstanceFieldRef(l, f.makeRef()), l2));
         }
         us.add(Jimple.v().newReturnVoidStmt());
       } else if (m.getName().equals("bootstrap$")) {
-        Local l = Jimple.v().newLocal("r", tclass.getType());
+        Local l = lc.generateLocal(tclass.getType());
         Value val = Jimple.v().newNewExpr(tclass.getType());
-        List<Value> capVals = new ArrayList<Value>();
         us.add(Jimple.v().newAssignStmt(l, val));
         us.add(Jimple.v().newInvokeStmt(Jimple.v().newSpecialInvokeExpr(l,
             Scene.v().makeConstructorRef(tclass, Collections.<Type>emptyList()), Collections.<Value>emptyList())));
         us.add(Jimple.v().newReturnStmt(l));
       } else {
-        Local this_ = Jimple.v().newLocal("r", tclass.getType());
+        Local this_ = lc.generateLocal(tclass.getType());
         us.add(Jimple.v().newIdentityStmt(this_, Jimple.v().newThisRef(tclass.getType())));
 
         List<Local> args = new ArrayList<Local>();
 
         for (SootField f : capFields) {
           int i = args.size();
-          Local l = Jimple.v().newLocal("c" + i, f.getType());
+          Local l = lc.generateLocal(f.getType());
           us.add(Jimple.v().newAssignStmt(l, Jimple.v().newInstanceFieldRef(this_, f.makeRef())));
           args.add(l);
         }
 
         for (Type ty : paramTypes) {
           int i = args.size();
-          Local l = Jimple.v().newLocal("a" + i, ty);
+          Local l = lc.generateLocal(ty);
           us.add(Jimple.v().newIdentityStmt(l, Jimple.v().newParameterRef(ty, i)));
           args.add(l);
         }
 
-        Local ret = Jimple.v().newLocal("r", retType);
-        us.add(Jimple.v().newAssignStmt(ret, Jimple.v().newStaticInvokeExpr(implMethod, args)));
+        final StaticInvokeExpr invoke = Jimple.v().newStaticInvokeExpr(implMethod, args);
+
+        if (retType == VoidType.v()) {
+          us.add(Jimple.v().newInvokeStmt(invoke));
+          us.add(Jimple.v().newReturnVoidStmt());
+        } else {
+          Local ret = lc.generateLocal(retType);
+          us.add(Jimple.v().newAssignStmt(ret, invoke));
+          us.add(Jimple.v().newReturnStmt(ret));
+        }
+      }
+      return jb;
+    }
+  }
+
+  private static class BridgeMethodSource implements MethodSource {
+    private final List<Type> paramTypes;
+    private final List<Type> targetTypes;
+    private final Type retType;
+    private final SootMethodRef apply;
+    private final Type targetRetType;
+
+    public BridgeMethodSource(List<Type> paramTypes, List<Type> targetTypes, Type retType, Type targetRetType,
+        SootMethodRef apply) {
+      Preconditions.checkArgument(paramTypes.size() == targetTypes.size());
+      this.paramTypes = paramTypes;
+      this.targetTypes = targetTypes;
+      this.retType = retType;
+      this.targetRetType = targetRetType;
+      this.apply = apply;
+    }
+
+    @Override
+    public Body getBody(SootMethod m, String phaseName) {
+      if (!phaseName.equals("jb"))
+        throw new Error("unsupported body type: " + phaseName);
+
+      SootClass tclass = m.getDeclaringClass();
+      JimpleBody jb = Jimple.v().newBody(m);
+      PatchingChain<Unit> us = jb.getUnits();
+      LocalGenerator lc = new LocalGenerator(jb);
+
+      Local this_ = lc.generateLocal(tclass.getType());
+      final IdentityStmt thisIdentity = Jimple.v().newIdentityStmt(this_, Jimple.v().newThisRef(tclass.getType()));
+      us.addFirst(thisIdentity);
+
+      List<Local> args = new ArrayList<Local>();
+
+      for (int i = 0; i < paramTypes.size(); i++) {
+        final Type paramType = paramTypes.get(i);
+        final Type targetType = targetTypes.get(i);
+
+        Local paramLocal = lc.generateLocal(paramType);
+        us.insertAfter(Jimple.v().newIdentityStmt(paramLocal, Jimple.v().newParameterRef(paramType, i)), thisIdentity);
+
+        final Local targetLocal = lc.generateLocal(targetType);
+        us.add(Jimple.v().newAssignStmt(targetLocal, Jimple.v().newCastExpr(paramLocal, targetType)));
+
+        args.add(targetLocal);
+      }
+
+      final VirtualInvokeExpr invoke = Jimple.v().newVirtualInvokeExpr(this_, apply, args);
+
+      if (retType == VoidType.v()) {
+        us.add(Jimple.v().newInvokeStmt(invoke));
+        us.add(Jimple.v().newReturnVoidStmt());
+      } else {
+        final Local nonCastRet = lc.generateLocal(targetRetType);
+        us.add(Jimple.v().newAssignStmt(nonCastRet, invoke));
+
+        final Local ret = lc.generateLocal(retType);
+        us.add(Jimple.v().newAssignStmt(ret, Jimple.v().newCastExpr(nonCastRet, retType)));
         us.add(Jimple.v().newReturnStmt(ret));
       }
+
       return jb;
     }
   }
