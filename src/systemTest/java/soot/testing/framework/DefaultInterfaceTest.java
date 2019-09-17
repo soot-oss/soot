f1fad7ef2c89f8c5086675a23e36fd4795ff719c
pavangb <38906373+pavanupb@users.noreply.github.com>
2019-09-17 14:10:10 +0530

    Initial-Setup

---
+++ src/systemTest/java/soot/testing/framework/DefaultInterfaceTest.java
@@ -0,0 +1,25 @@
+package soot.testing.framework;
+
+import org.junit.Test;
+
+import soot.Scene;
+import soot.SootMethod;
+import soot.jimple.toolkits.callgraph.CallGraph;
+import soot.jimple.toolkits.callgraph.ReachableMethods;
+
+public class DefaultInterfaceTest extends AbstractTestingFramework {
+	
+	@Test
+	public void interfaceTest()
+	{
+		String testClass = "soot.interfaceTesting.TestMain";
+
+	    final SootMethod target = prepareTarget(methodSigFromComponents(testClass, "void", "main"), testClass);
+	    
+	    final CallGraph cg = Scene.v().getCallGraph();
+	    
+	    final ReachableMethods methods = Scene.v().getReachableMethods();
+	}
+	
+
+}
