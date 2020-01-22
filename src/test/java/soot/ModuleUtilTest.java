157333df1c911a55a01884f99830bb1631154027
Andreas Dann <adann@mail.uni-paderborn.de>
2020-01-22 10:03:29 +0100

    add test cases for ModuleUtil and requires transitive clauses

--- src/test/java/soot/ModuleUtilTest.java
+++ src/test/java/soot/ModuleUtilTest.java
@@ -63,4 +63,66 @@ public class ModuleUtilTest {
         Assert.assertEquals("moduleA", foundModule);
 
     }
+
+
+    @Test
+    public void TwoLevelRequiresTransitiveExport(){
+        G.reset();
+        ModuleScene moduleScene = ModuleScene.v();
+
+        SootModuleInfo moduleA = new SootModuleInfo(SootModuleInfo.MODULE_INFO, "moduleA");
+        moduleA.addExportedPackage("de.upb");
+        moduleScene.addClassSilent(moduleA);
+
+        SootModuleInfo moduleB = new SootModuleInfo(SootModuleInfo.MODULE_INFO,"moduleB");
+        moduleB.getRequiredModules().put(moduleA,Modifier.REQUIRES_TRANSITIVE);
+        moduleScene.addClassSilent(moduleB);
+
+
+        SootModuleInfo moduleC = new SootModuleInfo(SootModuleInfo.MODULE_INFO,"moduleC");
+        moduleC.getRequiredModules().put(moduleB,Modifier.REQUIRES_TRANSITIVE);
+        moduleScene.addClassSilent(moduleC);
+
+
+        SootModuleInfo moduleD = new SootModuleInfo(SootModuleInfo.MODULE_INFO,"moduleD");
+        moduleD.getRequiredModules().put(moduleC,Modifier.REQUIRES_STATIC);
+        moduleScene.addClassSilent(moduleD);
+
+        ModuleUtil moduleUtil = ModuleUtil.v();
+        String foundModule = moduleUtil.findModuleThatExports("de.upb.A", "moduleD");
+        // output should be D, because module C, does NOT REQUIERS TRANSITIVE module B
+        Assert.assertEquals("moduleA", foundModule);
+
+    }
+
+    //the test should fail, as the requires transitive relations are not set in the module graph
+    @Test
+    public void TwoLevelRequiresTransitiveExportFailing(){
+        G.reset();
+        ModuleScene moduleScene = ModuleScene.v();
+
+        SootModuleInfo moduleA = new SootModuleInfo(SootModuleInfo.MODULE_INFO, "moduleA");
+        moduleA.addExportedPackage("de.upb");
+        moduleScene.addClassSilent(moduleA);
+
+        SootModuleInfo moduleB = new SootModuleInfo(SootModuleInfo.MODULE_INFO,"moduleB");
+        moduleB.getRequiredModules().put(moduleA,Modifier.REQUIRES_TRANSITIVE);
+        moduleScene.addClassSilent(moduleB);
+
+
+        SootModuleInfo moduleC = new SootModuleInfo(SootModuleInfo.MODULE_INFO,"moduleC");
+        moduleC.getRequiredModules().put(moduleB,Modifier.REQUIRES_STATIC);
+        moduleScene.addClassSilent(moduleC);
+
+
+        SootModuleInfo moduleD = new SootModuleInfo(SootModuleInfo.MODULE_INFO,"moduleD");
+        moduleD.getRequiredModules().put(moduleC,Modifier.REQUIRES_STATIC);
+        moduleScene.addClassSilent(moduleD);
+
+        ModuleUtil moduleUtil = ModuleUtil.v();
+        String foundModule = moduleUtil.findModuleThatExports("de.upb.A", "moduleD");
+        // output should be D, because module C, does NOT REQUIERS TRANSITIVE module B
+        Assert.assertEquals("moduleD", foundModule);
+
+    }
 }
