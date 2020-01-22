1c80b84e6ee9a77d7af9bb087ec31e71853b3baf
Andreas Dann <adann@mail.uni-paderborn.de>
2020-01-22 11:09:36 +0100

    fix test case

--- src/test/java/soot/ModuleUtilTest.java
+++ src/test/java/soot/ModuleUtilTest.java
@@ -33,6 +33,13 @@ public class ModuleUtilTest {
     public void ownPackage() {
         G.reset();
         ModuleUtil moduleUtil = ModuleUtil.v();
+        ModuleScene moduleScene = ModuleScene.v();
+
+        SootModuleInfo moduleA = new SootModuleInfo(SootModuleInfo.MODULE_INFO, "moduleA");
+        moduleA.addExportedPackage("de.upb");
+        moduleScene.addClassSilent(moduleA);
+
+
         String foundModule = moduleUtil.findModuleThatExports("de.upb", "moduleA");
         Assert.assertEquals("moduleA", foundModule);
     }
