509d8d411c587b85df340396d2110d51c33c196c
Manuel Benz <manuel.benz@codeshield.de>
2020-04-25 17:08:30 +0200

    fix src/systemTest/java/soot/jimple/PolymorphicDispatchTest.java

--- src/systemTest/java/soot/asm/AsmInnerClassTest.java
+++ src/systemTest/java/soot/asm/AsmInnerClassTest.java
@@ -28,11 +28,9 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 import soot.Modifier;
 import soot.Scene;
 import soot.SootMethod;
-import soot.options.Options;
 import soot.tagkit.InnerClassTag;
 import soot.tagkit.Tag;
 import soot.testing.framework.AbstractTestingFramework;
@@ -43,29 +41,30 @@ public class AsmInnerClassTest extends AbstractTestingFramework {
 
   @Override
   protected void setupSoot() {
-    Options.v().setPhaseOption("jb", "use-original-names:true");
-    Options.v().setPhaseOption("jb.tr", "ignore-nullpointer-dereferences:true");
-    Options.v().set_keep_line_number(true);
-    Options.v().setPhaseOption("cg.cha", "on");
+    // no need for call graph
   }
 
   @Test
   public void nonInner() {
     // statements at the beginning of a for loop should have the line number as for the branching
     // statement and not the last line number after the branch that leads outside the loop
-    SootMethod target = prepareTarget(methodSigFromComponents(TEST_TARGET_CLASS, "void", "method"), TEST_TARGET_CLASS);
+    SootMethod target =
+        prepareTarget(
+            methodSigFromComponents(TEST_TARGET_CLASS, "void", "method"), TEST_TARGET_CLASS);
     assertEquals(2, Scene.v().getApplicationClasses().size());
     assertFalse(target.getDeclaringClass().hasOuterClass());
     assertFalse(target.getDeclaringClass().isInnerClass());
     InnerClassTag tag = (InnerClassTag) target.getDeclaringClass().getTag("InnerClassTag");
-    //the class has inner classes
+    // the class has inner classes
     assertNotNull(tag);
   }
 
   @Test
   public void InnerStatic() {
-    SootMethod target2 = prepareTarget(methodSigFromComponents(TEST_TARGET_CLASS + "$Inner", "void", "<init>"),
-        TEST_TARGET_CLASS + "$Inner");
+    SootMethod target2 =
+        prepareTarget(
+            methodSigFromComponents(TEST_TARGET_CLASS + "$Inner", "void", "<init>"),
+            TEST_TARGET_CLASS + "$Inner");
     assertEquals(2, Scene.v().getApplicationClasses().size());
     assertTrue(target2.getDeclaringClass().hasOuterClass());
     assertTrue(target2.getDeclaringClass().isInnerClass());
@@ -78,29 +77,31 @@ public class AsmInnerClassTest extends AbstractTestingFramework {
 
   @Test
   public void InnerStaticInner() {
-    SootMethod target3 = prepareTarget(methodSigFromComponents(TEST_TARGET_CLASS + "$Inner$InnerInner", "void", "method"),
-        TEST_TARGET_CLASS + "$Inner$InnerInner");
+    SootMethod target3 =
+        prepareTarget(
+            methodSigFromComponents(TEST_TARGET_CLASS + "$Inner$InnerInner", "void", "method"),
+            TEST_TARGET_CLASS + "$Inner$InnerInner");
     // one dummy
     assertEquals(2, Scene.v().getApplicationClasses().size());
     assertTrue(target3.getDeclaringClass().hasOuterClass());
     assertTrue(target3.getDeclaringClass().isInnerClass());
     InnerClassTag innerClassTag = null;
-    for(Tag tag : target3.getDeclaringClass().getTags()){
-      //FIXME: we have multiple innerclasstags? for a parent it makes sense but for a child class?
-      if(tag instanceof InnerClassTag){
-        boolean inner=((InnerClassTag) tag).getInnerClass().equals("soot/asm/ScopeFinderTarget$Inner$InnerInner");
-        if(inner){
+    for (Tag tag : target3.getDeclaringClass().getTags()) {
+      // FIXME: we have multiple innerclasstags? for a parent it makes sense but for a child class?
+      if (tag instanceof InnerClassTag) {
+        boolean inner =
+            ((InnerClassTag) tag)
+                .getInnerClass()
+                .equals("soot/asm/ScopeFinderTarget$Inner$InnerInner");
+        if (inner) {
           innerClassTag = (InnerClassTag) tag;
           break;
         }
       }
-
-
     }
     assertNotNull(innerClassTag);
     assertEquals("soot/asm/ScopeFinderTarget$Inner$InnerInner", innerClassTag.getInnerClass());
     assertEquals("soot/asm/ScopeFinderTarget$Inner", innerClassTag.getOuterClass());
     assertFalse(Modifier.isStatic(innerClassTag.getAccessFlags()));
-
   }
 }
