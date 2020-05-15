995c3d01827104fa240bc7c5ff2b155af59f6317
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2020-05-16 01:35:51 +0200

    License

--- src/main/java/soot/jimple/toolkits/typing/fast/DefaultTypingStrategy.java
+++ src/main/java/soot/jimple/toolkits/typing/fast/DefaultTypingStrategy.java
@@ -1,5 +1,29 @@
 package soot.jimple.toolkits.typing.fast;
 
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 2008 Ben Bellamy 
+ * 
+ * All rights reserved.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -14,6 +38,9 @@ import soot.util.Chain;
 import soot.util.HashMultiMap;
 import soot.util.MultiMap;
 
+/**
+ * The defualt typing strategy
+ */
 public class DefaultTypingStrategy implements ITypingStrategy {
 
   public static final ITypingStrategy INSTANCE = new DefaultTypingStrategy();
@@ -54,6 +81,7 @@ public class DefaultTypingStrategy implements ITypingStrategy {
     return objectLikeVars;
   }
 
+  @Override
   public void minimize(List<Typing> tgs, IHierarchy h) {
     Set<Local> objectVars = getObjectLikeTypings(tgs);
     outer: for (ListIterator<Typing> i = tgs.listIterator(); i.hasNext();) {
