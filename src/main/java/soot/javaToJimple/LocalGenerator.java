2b85484e3ecceb2e7e3bb3d8e9d3c48f48b3cdaf
Waldo1637 <hoffman.timothy09@gmail.com>
2020-09-01 19:25:16 -0500

    Cleanup and optimize LocalGenerator

--- src/main/java/soot/javaToJimple/LocalGenerator.java
+++ src/main/java/soot/javaToJimple/LocalGenerator.java
@@ -23,131 +23,30 @@ package soot.javaToJimple;
  */
 
 import java.util.HashSet;
-import java.util.Set;
+
 import soot.Body;
+import soot.BooleanType;
+import soot.ByteType;
+import soot.CharType;
+import soot.DoubleType;
+import soot.FloatType;
+import soot.IntType;
 import soot.Local;
+import soot.LongType;
+import soot.RefLikeType;
+import soot.ShortType;
+import soot.Type;
+import soot.UnknownType;
+import soot.VoidType;
+import soot.jimple.Jimple;
 import soot.jimple.toolkits.typing.fast.Integer127Type;
 import soot.jimple.toolkits.typing.fast.Integer1Type;
 import soot.jimple.toolkits.typing.fast.Integer32767Type;
+import soot.util.Chain;
 
 public class LocalGenerator {
 
-  protected final soot.Body body;
-
-  public LocalGenerator(Body b) {
-    body = b;
-  }
-
-  protected transient Set<String> localNames = null;
-
-  protected boolean bodyContainsLocal(String name) {
-    return localNames.contains(name);
-  }
-
-  private void initLocalNames() {
-    localNames = new HashSet<String>();
-    for (Local l : body.getLocals()) {
-      localNames.add(l.getName());
-    }
-  }
-
-  /** generates a new soot local given the type */
-  public soot.Local generateLocal(soot.Type type) {
-
-    // store local names for enhanced performance
-    initLocalNames();
-
-    String name = "v";
-    if (type instanceof soot.IntType
-        || type instanceof Integer1Type
-        || type instanceof Integer127Type
-        || type instanceof Integer32767Type) {
-      while (true) {
-        name = nextIntName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.ByteType) {
-      while (true) {
-        name = nextByteName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.ShortType) {
-      while (true) {
-        name = nextShortName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.BooleanType) {
-      while (true) {
-        name = nextBooleanName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.VoidType) {
-      while (true) {
-        name = nextVoidName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.CharType) {
-      while (true) {
-        name = nextCharName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-      type = soot.CharType.v();
-    } else if (type instanceof soot.DoubleType) {
-      while (true) {
-        name = nextDoubleName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.FloatType) {
-      while (true) {
-        name = nextFloatName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.LongType) {
-      while (true) {
-        name = nextLongName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.RefLikeType) {
-      while (true) {
-        name = nextRefLikeTypeName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else if (type instanceof soot.UnknownType) {
-      while (true) {
-        name = nextUnknownTypeName();
-        if (!bodyContainsLocal(name)) {
-          break;
-        }
-      }
-    } else {
-      localNames = null;
-      throw new RuntimeException(
-          String.format("Unhandled Type %s of Local variable to Generate - Not Implemented", type.getClass().getName()));
-    }
-
-    localNames = null;
-    return createLocal(name, type);
-  }
+  protected final Chain<Local> locals;
 
   private int tempInt = -1;
   private int tempVoid = -1;
@@ -161,65 +60,121 @@ public class LocalGenerator {
   private int tempChar = -1;
   private int tempUnknownType = -1;
 
+  public LocalGenerator(Body b) {
+    this.locals = b.getLocals();
+  }
+
+  /** generates a new soot local given the type */
+  public Local generateLocal(Type type) {
+
+    // store local names for enhanced performance
+    HashSet<String> localNames = new HashSet<String>();
+    for (Local l : locals) {
+      localNames.add(l.getName());
+    }
+
+    String name;
+    if (type instanceof IntType || type instanceof Integer1Type || type instanceof Integer127Type
+        || type instanceof Integer32767Type) {
+      do {
+        name = nextIntName();
+      } while (localNames.contains(name));
+    } else if (type instanceof ByteType) {
+      do {
+        name = nextByteName();
+      } while (localNames.contains(name));
+    } else if (type instanceof ShortType) {
+      do {
+        name = nextShortName();
+      } while (localNames.contains(name));
+    } else if (type instanceof BooleanType) {
+      do {
+        name = nextBooleanName();
+      } while (localNames.contains(name));
+    } else if (type instanceof VoidType) {
+      do {
+        name = nextVoidName();
+      } while (localNames.contains(name));
+    } else if (type instanceof CharType) {
+      do {
+        name = nextCharName();
+      } while (localNames.contains(name));
+    } else if (type instanceof DoubleType) {
+      do {
+        name = nextDoubleName();
+      } while (localNames.contains(name));
+    } else if (type instanceof FloatType) {
+      do {
+        name = nextFloatName();
+      } while (localNames.contains(name));
+    } else if (type instanceof LongType) {
+      do {
+        name = nextLongName();
+      } while (localNames.contains(name));
+    } else if (type instanceof RefLikeType) {
+      do {
+        name = nextRefLikeTypeName();
+      } while (localNames.contains(name));
+    } else if (type instanceof UnknownType) {
+      do {
+        name = nextUnknownTypeName();
+      } while (localNames.contains(name));
+    } else {
+      throw new RuntimeException(
+          String.format("Unhandled Type %s of Local variable to Generate - Not Implemented", type.getClass().getName()));
+    }
+
+    return createLocal(name, type);
+  }
+
   private String nextIntName() {
-    tempInt++;
-    return "$i" + tempInt;
+    return "$i" + (++tempInt);
   }
 
   private String nextCharName() {
-    tempChar++;
-    return "$c" + tempChar;
+    return "$c" + (++tempChar);
   }
 
   private String nextVoidName() {
-    tempVoid++;
-    return "$v" + tempVoid;
+    return "$v" + (++tempVoid);
   }
 
   private String nextByteName() {
-    tempByte++;
-    return "$b" + tempByte;
+    return "$b" + (++tempByte);
   }
 
   private String nextShortName() {
-    tempShort++;
-    return "$s" + tempShort;
+    return "$s" + (++tempShort);
   }
 
   private String nextBooleanName() {
-    tempBoolean++;
-    return "$z" + tempBoolean;
+    return "$z" + (++tempBoolean);
   }
 
   private String nextDoubleName() {
-    tempDouble++;
-    return "$d" + tempDouble;
+    return "$d" + (++tempDouble);
   }
 
   private String nextFloatName() {
-    tempFloat++;
-    return "$f" + tempFloat;
+    return "$f" + (++tempFloat);
   }
 
   private String nextLongName() {
-    tempLong++;
-    return "$l" + tempLong;
+    return "$l" + (++tempLong);
   }
 
   private String nextRefLikeTypeName() {
-    tempRefLikeType++;
-    return "$r" + tempRefLikeType;
+    return "$r" + (++tempRefLikeType);
   }
 
   private String nextUnknownTypeName() {
-    tempUnknownType++;
-    return "$u" + tempUnknownType;
+    return "$u" + (++tempUnknownType);
   }
 
   // this should be used for generated locals only
-  protected soot.Local createLocal(String name, soot.Type sootType) {
-    soot.Local sootLocal = soot.jimple.Jimple.v().newLocal(name, sootType);
-    body.getLocals().add(sootLocal);
+  protected Local createLocal(String name, Type sootType) {
+    Local sootLocal = Jimple.v().newLocal(name, sootType);
+    locals.add(sootLocal);
     return sootLocal;
   }
 }
