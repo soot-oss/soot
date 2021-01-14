35715aa1acfbd0f82bc851a6f79779766875ac3a
FXTi <zjxiang1998@gmail.com>
2021-01-14 22:52:52 +0800

    Fix style

--- src/main/java/soot/jimple/toolkits/scalar/ConditionalConstantPropagatorAndFolder.java
+++ src/main/java/soot/jimple/toolkits/scalar/ConditionalConstantPropagatorAndFolder.java
@@ -1,14 +1,35 @@
 package soot.jimple.toolkits.scalar;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+/*-
+ * #%L
+ * Soot - a J*va Optimization Framework
+ * %%
+ * Copyright (C) 1997 - 2018 Raja Vallée-Rai and others
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import soot.Body;
 import soot.BodyTransformer;
@@ -261,7 +282,8 @@ class CCPFAnalysis extends ForwardBranchedFlowAnalysis<HashMap<Local, Constant>>
         HashMap<Local, Constant> oneBranch = null;
         Unit unitBranch = null;
 
-        IFSTMT: {
+        IFSTMT:
+        {
             if (s instanceof IfStmt) {
                 IfStmt ifStmt = (IfStmt) s;
                 Value cond = ifStmt.getCondition();
@@ -297,7 +319,8 @@ class CCPFAnalysis extends ForwardBranchedFlowAnalysis<HashMap<Local, Constant>>
             }
         } // end IFSTMT
 
-        TABLESWITCHSTMT: {
+        TABLESWITCHSTMT:
+        {
             if (s instanceof TableSwitchStmt) {
                 TableSwitchStmt table = (TableSwitchStmt) s;
                 Value keyV = table.getKey();
@@ -341,7 +364,8 @@ class CCPFAnalysis extends ForwardBranchedFlowAnalysis<HashMap<Local, Constant>>
             }
         } // end TABLESWITCHSTMT
 
-        LOOKUPSWITCHSTMT: {
+        LOOKUPSWITCHSTMT:
+        {
             if (s instanceof LookupSwitchStmt) {
                 LookupSwitchStmt lookup = (LookupSwitchStmt) s;
                 Value keyV = lookup.getKey();
@@ -422,7 +446,9 @@ class CCPFAnalysis extends ForwardBranchedFlowAnalysis<HashMap<Local, Constant>>
         if (firstRun) {
             // Do the true init for throw-handlers.
             for (Trap trap : ((UnitGraph) graph).getBody().getTraps()) {
-                HashMap<Local, Constant> res = new HashMap() {{ copy(handlerMap, this); }};
+                HashMap<Local, Constant> res = new HashMap() {{
+                    copy(handlerMap, this);
+                }};
                 unitToBeforeFlow.put(trap.getHandlerUnit(), res);
             }
             firstRun = false;
