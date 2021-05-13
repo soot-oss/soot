65afb1376829a0ac7e19e7ceaed389b2ce819387
Linghui Luo <linghui@outlook.de>
2021-05-13 18:47:49 +0200

    format

--- src/test/java/soot/EntryPointsTest.java
+++ src/test/java/soot/EntryPointsTest.java
@@ -20,6 +20,7 @@
  * #L%
  */
 package soot;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -37,7 +38,8 @@ import soot.options.Options;
 
 /**
  * Tests the EntryPoints class
- * @author Linghui Luo 
+ * 
+ * @author Linghui Luo
  */
 public class EntryPointsTest {
 
@@ -50,20 +52,19 @@ public class EntryPointsTest {
 		Options.v().set_src_prec(Options.src_prec_class);
 		Options.v().set_allow_phantom_refs(true);
 		Options.v().set_ignore_resolving_levels(true);
-	    Options.v().setPhaseOption("cg.spark", "on");
-	    Options.v().setPhaseOption("cg.spark", "string-constants:true");
-	    Options.v().set_whole_program(true);
-		Scene.v()
-        .loadNecessaryClasses();
-	    SootMethod mainMethod = Scene.v().getMainMethod();
-	    Scene.v().setEntryPoints(Collections.singletonList(mainMethod));
-	    PackManager.v().getPack("cg").apply();
+		Options.v().setPhaseOption("cg.spark", "on");
+		Options.v().setPhaseOption("cg.spark", "string-constants:true");
+		Options.v().set_whole_program(true);
+		Scene.v().loadNecessaryClasses();
+		SootMethod mainMethod = Scene.v().getMainMethod();
+		Scene.v().setEntryPoints(Collections.singletonList(mainMethod));
+		PackManager.v().getPack("cg").apply();
 		CallGraph cg = Scene.v().getCallGraph();
 		boolean found = false;
-		for(Edge edge: cg) {
-			if (edge.getSrc().method().getSignature().equals("<soot.Main: void main(java.lang.String[])>")){
+		for (Edge edge : cg) {
+			if (edge.getSrc().method().getSignature().equals("<soot.Main: void main(java.lang.String[])>")) {
 				if (edge.getTgt().method().getSignature().equals("<soot.A: void <clinit>()>")) { // A1 is used in main
-					found = true; 
+					found = true;
 					break;
 				}
 			}
@@ -73,9 +74,13 @@ public class EntryPointsTest {
 		SootClass a = Scene.v().getSootClassUnsafe("soot.A");
 		assertTrue(a1 != null);
 		List<String> clinits1 = new ArrayList<>();
-		EntryPoints.v().clinitsOf(a1).forEach(e -> {clinits1.add(e.toString());});
+		EntryPoints.v().clinitsOf(a1).forEach(e -> {
+			clinits1.add(e.toString());
+		});
 		List<String> clinits = new ArrayList<>();
-		EntryPoints.v().clinitsOf(a).forEach(e -> {clinits.add(e.toString());});
+		EntryPoints.v().clinitsOf(a).forEach(e -> {
+			clinits.add(e.toString());
+		});
 		assertEquals(clinits1, clinits);
 	}
 }
