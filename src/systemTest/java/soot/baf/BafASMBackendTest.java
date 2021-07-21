a06e6ad251574224a65da18dcf125181e87bef67
Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
2021-07-21 18:34:30 -0500

    fix the test case Java11 failure

--- src/systemTest/java/soot/baf/BafASMBackendTest.java
+++ src/systemTest/java/soot/baf/BafASMBackendTest.java
@@ -22,14 +22,8 @@ package soot.baf;
  * #L%
  */
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,10 +31,6 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.util.CheckClassAdapter;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 
-import soot.G;
-import soot.ModulePathSourceLocator;
-import soot.ModuleScene;
-import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.options.Options;
@@ -56,14 +46,14 @@ public class BafASMBackendTest extends AbstractTestingFramework {
 
   @Override
   protected void setupSoot() {
+      final Options opts = Options.v();
+      opts.set_derive_java_version(false);
   }
 
   @Test
   public void testCachingInvalidation() throws Exception {
     final String targetClass = "soot.baf.BafASMBackendTestInput_Default";
-    final Scene scene = customSetupLib(rtJar(), Collections.singletonList(targetClass));
-
-    SootClass sc = getOrResolveSootClass(scene, targetClass, SootClass.BODIES);
+    SootClass sc = prepareTarget("<" + targetClass + ": void <init>()>", targetClass).getDeclaringClass();
     Assert.assertNotNull(sc);
     Assert.assertEquals(targetClass, sc.getName());
 
@@ -82,45 +72,5 @@ public class BafASMBackendTest extends AbstractTestingFramework {
     CheckClassAdapter.verify(new ClassReader(bytecode), null, false, new PrintWriter(strWriter));
     String verifyMsg = strWriter.toString();
     Assert.assertTrue(verifyMsg, verifyMsg.isEmpty());
-  }
-
-  private Scene customSetupLib(String rtJar, List<String> classNames) {
-    G.reset();
-
-    final Options opts = Options.v();
-
-    opts.set_whole_program(true);
-    opts.set_output_format(Options.output_format_none);
-    opts.set_allow_phantom_refs(true);
-    opts.set_no_bodies_for_excluded(true);
-    opts.set_exclude(getExcludes());
-    opts.set_include(classNames);
-    opts.set_process_dir(Collections.singletonList(SYSTEMTEST_TARGET_CLASSES_DIR));
-
-    // Disable deriving Java version from the input bytecode so that the
-    // version is computed based only on the logic in BafASMBackend.
-    opts.set_derive_java_version(false);
-
-    final boolean isModuleJar = isModuleJar(rtJar);
-    if (isModuleJar) {
-      opts.set_soot_modulepath(ModulePathSourceLocator.DUMMY_CLASSPATH_JDK9_FS);
-    } else {
-      opts.set_soot_classpath(rtJar);
-    }
-
-    // NOTE: must obtain Scene after all options are set
-    Scene scene = isModuleJar ? ModuleScene.v() : Scene.v();
-    scene.loadNecessaryClasses();
-    runSoot();
-    return scene;
-  }
-
-  private static String rtJar() throws IOException {
-    Path p = Paths.get(System.getProperty("java.home"), "lib", "rt.jar");
-    return Files.exists(p) ? p.toRealPath().toString() : null;
-  }
-
-  private static boolean isModuleJar(String rtJar) {
-    return (rtJar == null);
   }
 }
