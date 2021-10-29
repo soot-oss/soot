89894d72b8e25726200c1e7f0fd6560f8ac12372
Steven Arzt <Steven.Arzt@sit.fraunhofer.de>
2021-10-29 16:46:50 +0200

    checkstyle again

--- src/main/java/soot/jimple/toolkits/callgraph/VirtualEdgesSummaries.java
+++ src/main/java/soot/jimple/toolkits/callgraph/VirtualEdgesSummaries.java
@@ -550,12 +550,15 @@ public class VirtualEdgesSummaries {
 
     @Override
     public boolean equals(Object obj) {
-      if (this == obj)
+      if (this == obj) {
         return true;
-      if (!super.equals(obj))
+      }
+      if (!super.equals(obj)) {
         return false;
-      if (getClass() != obj.getClass())
+      }
+      if (getClass() != obj.getClass()) {
         return false;
+      }
       return true;
     }
   }
