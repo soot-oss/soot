52290e2e1bf34338a0448b1f1e7a736304045ed4
Justus Kluge <justus.kluge@sit.fraunhofer.de>
2022-07-01 02:26:48 +0200

    Added Info Case

--- src/main/java/soot/dotnet/members/DotnetMethod.java
+++ src/main/java/soot/dotnet/members/DotnetMethod.java
@@ -278,6 +278,7 @@ public class DotnetMethod extends AbstractDotnetMember {
           method.addTag(new DeprecatedTag());
         }
       } catch (Exception ignore) {
+        logger.info("Ignores", ignore);
       }
     }
 
