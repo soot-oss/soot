a4ee7ea4f507b6e1e5b36684d0c906369122b2e5
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2022-12-23 15:23:51 +0100

    Allow implementations to be used

--- src/main/java/soot/toolkits/scalar/SimpleLocalDefs.java
+++ src/main/java/soot/toolkits/scalar/SimpleLocalDefs.java
@@ -49,10 +49,10 @@ import soot.toolkits.graph.UnitGraph;
  */
 public class SimpleLocalDefs implements LocalDefs {
 
-  private static class StaticSingleAssignment implements LocalDefs {
+  public static class StaticSingleAssignment implements LocalDefs {
     final Map<Local, List<Unit>> result;
 
-    StaticSingleAssignment(Local[] locals, List<Unit>[] unitList) {
+    public StaticSingleAssignment(Local[] locals, List<Unit>[] unitList) {
       final int N = locals.length;
       assert (N == unitList.length);
 
@@ -79,7 +79,7 @@ public class SimpleLocalDefs implements LocalDefs {
     }
   } // end inner class StaticSingleAssignment
 
-  private class FlowAssignment extends ForwardFlowAnalysis<Unit, FlowAssignment.FlowBitSet> implements LocalDefs {
+  public class FlowAssignment extends ForwardFlowAnalysis<Unit, FlowAssignment.FlowBitSet> implements LocalDefs {
 
     class FlowBitSet extends BitSet {
       private static final long serialVersionUID = -8348696077189400377L;
@@ -137,7 +137,7 @@ public class SimpleLocalDefs implements LocalDefs {
 
     private Map<Unit, Integer> indexOfUnit;
 
-    FlowAssignment(DirectedGraph<Unit> graph, Local[] locals, List<Unit>[] unitList, int units, boolean omitSSA) {
+    public FlowAssignment(DirectedGraph<Unit> graph, Local[] locals, List<Unit>[] unitList, int units, boolean omitSSA) {
       super(graph);
       this.unitList = unitList;
       this.universe = new Unit[units];
