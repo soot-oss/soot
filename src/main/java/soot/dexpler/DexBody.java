8cb447c02068b43753309bb50d9a8163d923002e
Steven Arzt <steven.arzt@sit.fraunhofer.de>
2024-10-18 16:48:56 +0200

    added a missing import

--- src/main/java/soot/dexpler/DexBody.java
+++ src/main/java/soot/dexpler/DexBody.java
@@ -38,6 +38,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
