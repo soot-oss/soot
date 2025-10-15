9b5726bd49ea8e10f246cc081393d0237793c91e
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2025-10-15 08:37:01 +0200

    Formatting

--- src/main/java/soot/Dependencies.java
+++ src/main/java/soot/Dependencies.java
@@ -26,15 +26,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Dependencies {
-    public final Set<Type> typesToHierarchy, typesToSignature;
+  public final Set<Type> typesToHierarchy, typesToSignature;
 
-    public Dependencies() {
-      typesToHierarchy = new HashSet<>();
-      typesToSignature = new HashSet<>();
-    }
-
-    public Dependencies(Set<Type> typesToHierarchy, Set<Type> typesToSignature) {
-      this.typesToHierarchy = typesToHierarchy == null ? new HashSet<>() : typesToHierarchy;
-      this.typesToSignature = typesToSignature == null ? new HashSet<>() : typesToSignature;
-    }
+  public Dependencies() {
+    typesToHierarchy = new HashSet<>();
+    typesToSignature = new HashSet<>();
   }
+
+  public Dependencies(Set<Type> typesToHierarchy, Set<Type> typesToSignature) {
+    this.typesToHierarchy = typesToHierarchy == null ? new HashSet<>() : typesToHierarchy;
+    this.typesToSignature = typesToSignature == null ? new HashSet<>() : typesToSignature;
+  }
+}
