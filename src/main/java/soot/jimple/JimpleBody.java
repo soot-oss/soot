6f7542862505b50fc1292cfe3caaa9b965f972ed
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2025-11-25 13:01:08 +0100

    Smaller fixes

--- src/main/java/soot/jimple/JimpleBody.java
+++ src/main/java/soot/jimple/JimpleBody.java
@@ -101,7 +101,7 @@ public class JimpleBody extends StmtBody {
   @Override
   public Object clone(boolean noLocalsClone) {
     Body b = new JimpleBody(getMethod());
-    b.importBodyContentsFrom(this, true);
+    b.importBodyContentsFrom(this, noLocalsClone);
     return b;
   }
 
