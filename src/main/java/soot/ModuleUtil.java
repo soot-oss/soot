e1d8805379a30f85b991890259f25cfc09af59d6
Steven Arzt <steven.arzt@sit.fraunhofer.de>
2026-01-14 12:10:40 +0100

    fixed imports

--- src/main/java/soot/ModuleUtil.java
+++ src/main/java/soot/ModuleUtil.java
@@ -30,6 +30,7 @@ import com.google.common.cache.LoadingCache;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
