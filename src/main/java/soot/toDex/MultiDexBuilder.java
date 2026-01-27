5c44259f2849003abb1ea55c1971ac8932e74884
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2026-01-27 18:23:19 +0100

    Dex Overflow: Log a warning instead of throwing an exception https://github.com/soot-oss/soot/pull/2048

--- src/main/java/soot/toDex/MultiDexBuilder.java
+++ src/main/java/soot/toDex/MultiDexBuilder.java
@@ -33,10 +33,16 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import soot.asm.AsmJava9ClassProvider;
+
 /**
  * @author Manuel Benz created on 26.09.17
  */
 public class MultiDexBuilder {
+  private static final Logger logger = LoggerFactory.getLogger(MultiDexBuilder.class);
 
   protected final Opcodes opcodes;
   protected final List<DexPool> dexPools = new LinkedList<>();
@@ -93,7 +99,8 @@ public class MultiDexBuilder {
     // (https://developer.android.com/studio/build/multidex.html,
     // http://www.fasteque.com/deep-dive-into-android-multidex/)
     if (!opcodes.isArt()) {
-      throw new RuntimeException("Dex file overflow. Splitting not support for pre Lollipop Android (Api 22).");
+      logger.warn("Dex file overflow. Splitting is not supported for any version earlier than"
+          + "Lollipop Android (API 22). The application will not run on older devices.");
     }
 
     return true;
