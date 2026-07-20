700a95486e18da27b60d1796fff827ddc65605c5
Danish-git123 <danishbagwan88@gmail.com>
2026-07-20 16:41:51 +0530

    style edits

--- src/main/java/soot/jimple/toolkits/callgraph/VirtualCallSiteFilter.java
+++ src/main/java/soot/jimple/toolkits/callgraph/VirtualCallSiteFilter.java
@@ -28,6 +28,5 @@ import soot.Type;
 //Act as a  bridge interface so whichever class comes here needs to implement this interface
 // Extension point for filtering out reaching types at virtual call sites that model special framework control flow
 public interface VirtualCallSiteFilter {
-
   boolean skipSite(VirtualCallSite site, FastHierarchy fh, Type type);
 }
